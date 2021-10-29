b0e759e8ce4ac1373f48af4e0f2e75b946fccc50
Adam Kundrat <adam@kundrat.eu>
2021-10-28 21:31:56 -0700

    fixed github actions and imports.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,11 @@
-import { firestore, initializeApp } from "firebase-admin";
+import * as admin from 'firebase-admin';
 import * as functions from "firebase-functions";
 import { merge } from 'lodash';
 import Web3 from 'web3';
 import { Member } from '../interfaces/models/member';
 import { CREATE_MEMBER_IF_NOT_EXISTS, WEN_FUNCTIONS } from './../interfaces/functions/index';
 import { DOCUMENTS } from './../interfaces/models/base';
-initializeApp();
+admin.initializeApp();
 
 exports[WEN_FUNCTIONS.CREATE_MEMBER_IF_NOT_EXISTS] = functions.https.onCall(async (data: CREATE_MEMBER_IF_NOT_EXISTS): Promise<Member> => {
   // Validate address.
@@ -13,17 +13,17 @@ exports[WEN_FUNCTIONS.CREATE_MEMBER_IF_NOT_EXISTS] = functions.https.onCall(asyn
     throw new Error('Invalid address');
   }
 
-  let docMember = await firestore().collection(DOCUMENTS.MEMBER).doc(data.address).get();
+  let docMember = await admin.firestore().collection(DOCUMENTS.MEMBER).doc(data.address).get();
   if (!docMember.exists) {
     // Document does not exists. We must create the member.
-    await firestore().collection(DOCUMENTS.MEMBER).doc(data.address).set({
+    await admin.firestore().collection(DOCUMENTS.MEMBER).doc(data.address).set({
       uid: data.address,
-      createdOn: firestore.Timestamp.now(),
-      updatedOn: firestore.Timestamp.now()
+      createdOn: admin.firestore.Timestamp.now(),
+      updatedOn: admin.firestore.Timestamp.now()
     });
 
     // Load latest
-    docMember = await firestore().collection(DOCUMENTS.MEMBER).doc(data.address).get();
+    docMember = await admin.firestore().collection(DOCUMENTS.MEMBER).doc(data.address).get();
   }
 
   // Return member.
@@ -36,17 +36,17 @@ exports[WEN_FUNCTIONS.UPDATE_MEMBER_IF_NOT_EXISTS] = functions.https.onCall(asyn
     throw new Error('Invalid address');
   }
 
-  let docMember = await firestore().collection(DOCUMENTS.MEMBER).doc(data.uid).get();
+  let docMember = await admin.firestore().collection(DOCUMENTS.MEMBER).doc(data.uid).get();
   if (!docMember.exists) {
     throw new Error('Member does not exists');
   }
 
-  await firestore().collection(DOCUMENTS.MEMBER).doc(data.uid).update(merge(data, {
-    updatedOn: firestore.Timestamp.now()
+  await admin.firestore().collection(DOCUMENTS.MEMBER).doc(data.uid).update(merge(data, {
+    updatedOn: admin.firestore.Timestamp.now()
   }));
 
   // Load latest
-  docMember = await firestore().collection(DOCUMENTS.MEMBER).doc(data.uid).get();
+  docMember = await admin.firestore().collection(DOCUMENTS.MEMBER).doc(data.uid).get();
 
   // Return member.
   return <Member>docMember.data();
