e6fe1778136b975c4e46c92190b66f2f6f244f6f
Lukas Horak <horaklukas@centrum.cz>
2021-11-02 21:06:20 +0100

    ⚡️ Add change dection onpush for sider component

--- src/app/@shell/ui/sider/sider.component.ts
+++ src/app/@shell/ui/sider/sider.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ThemeService } from '@core/services/theme';
 import { ROUTER_UTILS } from '@core/utils/router.utils';
 
 @Component({
   selector: 'wen-sider',
   templateUrl: './sider.component.html',
-  styleUrls: ['./sider.component.less']
+  styleUrls: ['./sider.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SiderComponent {
   path = ROUTER_UTILS.config.base;
