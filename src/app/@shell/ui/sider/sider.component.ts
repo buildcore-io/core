9359088ddc454ec2451c5e11991c0d43f266a1ea
Lukas Horak <horaklukas@centrum.cz>
2021-11-03 10:46:59 +0100

    🐛 Preserver active menu item after page reload

--- src/app/@shell/ui/sider/sider.component.ts
+++ src/app/@shell/ui/sider/sider.component.ts
@@ -9,7 +9,12 @@ import { ROUTER_UTILS } from '@core/utils/router.utils';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SiderComponent {
-  routes = ROUTER_UTILS.config;
+  homeRoute = ROUTER_UTILS.config.base.home 
+  menuItems = [
+    { route: ROUTER_UTILS.config.member.profile, label: 'My profile' },
+    { route: ROUTER_UTILS.config.space.root, label: 'Discover' },
+    { route: ROUTER_UTILS.config.base.dashboard, label: 'Overview' },
+  ]
 
   constructor(public themeService: ThemeService) { }
 }
