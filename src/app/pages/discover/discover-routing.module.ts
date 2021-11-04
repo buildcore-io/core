d6b16f11b9ac18d1dc9069acd062f92cc2d27127
Lukas Horak <horaklukas@centrum.cz>
2021-11-04 17:48:45 +0100

    ✨ Add proposals page

--- src/app/pages/discover/discover-routing.module.ts
+++ src/app/pages/discover/discover-routing.module.ts
@@ -4,6 +4,7 @@ import { ROUTER_UTILS } from '@core/utils/router.utils';
 import { AwardsPage } from './pages/awards/awards.page';
 import { DiscoverPage } from './pages/discover/discover.page';
 import { MembersPage } from './pages/members/members.page';
+import { ProposalsPage } from './pages/proposals/proposals.page';
 import { SpacesPage } from './pages/spaces/spaces.page';
 
 const routes: Routes = [
@@ -12,6 +13,7 @@ const routes: Routes = [
     children: [
       { path: ROUTER_UTILS.config.discover.spaces, component: SpacesPage, },
       { path: ROUTER_UTILS.config.discover.awards, component: AwardsPage, },
+      { path: ROUTER_UTILS.config.discover.proposals, component: ProposalsPage, },
       { path: ROUTER_UTILS.config.discover.members, component: MembersPage, }
     ]
   },
