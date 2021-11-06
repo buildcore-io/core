1d453b10761b001b647df07dfd77b19ab2f4761b
Adam Kundrat <adam@kundrat.eu>
2021-11-05 21:11:16 -0700

    Avatar minting SC

--- protocol/test/index.ts
+++ protocol/test/index.ts
@@ -17,3 +17,23 @@ describe("Greeter", function () {
     expect(await greeter.greet()).to.equal("Hola, mundo!");
   });
 });
+
+describe("Minting the token and returning it", function () {
+  it("should the contract be able to mint a function and return it", async function () {
+    const metadata = "IFPS_ADDRESS" // Metadata URL will be stored in IPFS
+
+    const FactoryContract = await ethers.getContractFactory("AvatarNft");
+    const factoryContract = await FactoryContract.deploy();
+
+    const transaction = await factoryContract.createToken(metadata);
+    const tx = await transaction.wait();
+
+    const event = tx.events![0];
+    const value = event.args![2];
+    const tokenId = value.toNumber();
+
+    const tokenURI = await factoryContract.tokenURI(tokenId);
+    expect(tokenURI).to.be.equal(metadata);
+
+  });
+});
