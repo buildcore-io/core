2a7a19ccdaf127a4838a183250cadc621541bba5
Lukas Horak <horaklukas@centrum.cz>
2021-11-10 10:43:28 +0100

    🤡 Mock space card on spaces page

--- src/app/components/spaces/components/space-card/space-card.component.spec.ts
+++ src/app/components/spaces/components/space-card/space-card.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { IconModule } from './../../../../components/ui/components/icon/icon.module';
 import { SpaceCardComponent } from './space-card.component';
+
 
 describe('SpaceCardComponent', () => {
   let component: SpaceCardComponent;
@@ -8,9 +10,10 @@ describe('SpaceCardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SpaceCardComponent ]
+      declarations: [SpaceCardComponent],
+      imports: [IconModule, NzAvatarModule]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
