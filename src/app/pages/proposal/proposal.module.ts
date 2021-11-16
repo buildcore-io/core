41e840bffb6ee5492553bc5f0b41a15babfcf6d3
Josef Frnka <josef@infinetcloud.com>
2021-11-16 12:50:26 +0100

    ✨ proposal detail added

--- src/app/pages/proposal/proposal.module.ts
+++ src/app/pages/proposal/proposal.module.ts
@@ -1,11 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import { NzGridModule } from 'ng-zorro-antd/grid';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzProgressModule } from 'ng-zorro-antd/progress';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzTagModule } from 'ng-zorro-antd/tag';
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
 import { NewPage } from './pages/new/new.page';
 import { OverviewPage } from './pages/overview/overview.page';
 import { ProposalPage } from './pages/proposal/proposal.page';
@@ -25,6 +32,13 @@ import { ProposalRoutingModule } from './proposal-routing.module';
     NzCardModule,
     NzIconModule,
     NzInputModule,
+    NzAvatarModule,
+    NzGridModule,
+    NzMenuModule,
+    NzTypographyModule,
+    NzProgressModule,
+    NzTableModule,
+    NzTagModule,
     NzDatePickerModule
   ]
 })
