e9c0e3f93332449c3987976eed88e6b852403138
Josef Frnka <josef@infinetcloud.com>
2021-11-16 10:54:26 +0100

    ✨ new proposal layout

--- src/app/pages/proposal/pages/new/new.page.ts
+++ src/app/pages/proposal/pages/new/new.page.ts
@@ -1,4 +1,6 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewChild } from '@angular/core';
+import { ROUTER_UTILS } from '@core/utils/router.utils';
+import { NzDatePickerComponent } from 'ng-zorro-antd/date-picker';
 
 @Component({
   selector: 'wen-new',
@@ -7,4 +9,39 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NewPage {
+
+  public get urlToSpaces(): string {
+    return '/' + ROUTER_UTILS.config.discover.root + '/' + ROUTER_UTILS.config.discover.spaces;
+  }
+
+  // TODO default date picker behaviour feel free to remove it
+  startValue: Date | null = null;
+  endValue: Date | null = null;
+  @ViewChild('endDatePicker') endDatePicker!: NzDatePickerComponent;
+
+  disabledStartDate = (startValue: Date): boolean => {
+    if (!startValue || !this.endValue) {
+      return false;
+    }
+    return startValue.getTime() > this.endValue.getTime();
+  };
+
+  disabledEndDate = (endValue: Date): boolean => {
+    if (!endValue || !this.startValue) {
+      return false;
+    }
+    return endValue.getTime() <= this.startValue.getTime();
+  };
+
+  handleStartOpenChange(open: boolean): void {
+    if (!open) {
+      this.endDatePicker.open();
+    }
+    console.log('handleStartOpenChange', open);
+  }
+
+  handleEndOpenChange(open: boolean): void {
+    console.log('handleEndOpenChange', open);
+  }
+
 }
--- src/app/pages/proposal/proposal.module.ts
+++ src/app/pages/proposal/proposal.module.ts
@@ -1,11 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzInputModule } from 'ng-zorro-antd/input';
 import { NewPage } from './pages/new/new.page';
 import { OverviewPage } from './pages/overview/overview.page';
 import { ProposalPage } from './pages/proposal/proposal.page';
 import { ProposalRoutingModule } from './proposal-routing.module';
-
-
 
 @NgModule({
   declarations: [
@@ -15,7 +19,13 @@ import { ProposalRoutingModule } from './proposal-routing.module';
   ],
   imports: [
     CommonModule,
-    ProposalRoutingModule
+    ProposalRoutingModule,
+    ReactiveFormsModule,
+    NzButtonModule,
+    NzCardModule,
+    NzIconModule,
+    NzInputModule,
+    NzDatePickerModule
   ]
 })
 export class ProposalModule { }
