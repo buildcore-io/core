e39d5f685a7cdba71bfc239480e8cd4ff1721e6d
Lukas Horak <horaklukas@centrum.cz>
2021-11-18 09:27:48 +0100

    🔧 Define colors according to the design system

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,28 @@ module.exports = {
     extend: {
       color: {
         inherit: 'inherit',
+        orange: {
+          lightest: '#F2C50C',
+          light: '#FFA319',
+          dark: '#F39200',
+          DEFAULT: '#F39200',
+          darkest: '#F2550C',
+        },
+        blue: {
+          light1: '#008AF2',
+          light2: '#1895F2',
+          medium: '#005FA6',
+          DEFAULT: '#005FA6',
+          dark1: '#0C16F2',
+          dark2: '#1822F2',
+          darkest: '#1117A6',
+        },
+        green: {
+          dark: '#11A696',
+          neon: '#58F218',
+        },
+        brown: '#A66300',
+        pink: '#F20CDF',
       },
       spacing: {
         75: '18.75rem',
