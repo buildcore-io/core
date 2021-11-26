47174f1ed83f028e8eae876323509f410646e558
Josef Frnka <josef@infinetcloud.com>
2021-11-26 17:14:20 +0100

    menu popover added

--- src/app/@shell/ui/sider/menu/menu.module.ts
+++ src/app/@shell/ui/sider/menu/menu.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzPopoverModule } from 'ng-zorro-antd/popover';
 import { MenuItemDirective } from './menu-item.directive';
 import { MenuComponent } from './menu.component';
 
@@ -11,6 +12,7 @@ import { MenuComponent } from './menu.component';
     CommonModule,
     RouterModule,
     NzMenuModule,
+    NzPopoverModule
   ],
   exports: [MenuComponent]
 })
