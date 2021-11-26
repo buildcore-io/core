5c0a04e568f7aca14ff5ac02640e48ff59c8c709
Adam Kundrat <adam@kundrat.eu>
2021-11-25 16:23:52 -0800

    tweaked header

--- src/app/@shell/ui/header/header.component.ts
+++ src/app/@shell/ui/header/header.component.ts
@@ -1,22 +1,45 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '@components/auth/services/auth.service';
 import { ROUTER_UTILS } from '@core/utils/router.utils';
-import { BehaviorSubject } from 'rxjs';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { BehaviorSubject, firstValueFrom } from 'rxjs';
 import { Member } from './../../../../../functions/interfaces/models/member';
+import { Space } from './../../../../../functions/interfaces/models/space';
+import { MemberApi } from './../../../@api/member.api';
 
+@UntilDestroy()
 @Component({
   selector: 'wen-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.less'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   public path = ROUTER_UTILS.config.base;
+  public enableCreateAwardProposal = false;
+
   constructor(
     private router: Router,
+    private memberApi: MemberApi,
+    private cd: ChangeDetectorRef,
     public auth: AuthService
   ) {}
+
+  public ngOnInit(): void {
+    this.member$.pipe(
+      untilDestroyed(this)
+    ).subscribe(async (obj) => {
+      if (obj?.uid) {
+        const space: Space[] = await firstValueFrom(this.memberApi.topSpaces(obj.uid, 'createdOn', undefined, 1));
+        this.enableCreateAwardProposal = space.length > 0;
+      } else {
+        this.enableCreateAwardProposal = false;
+      }
+
+      this.cd.markForCheck();
+    });
+  }
 
   public get isLoggedIn$(): BehaviorSubject<boolean> {
     return this.auth.isLoggedIn$;
