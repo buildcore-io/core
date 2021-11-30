fbd5c698becd5d6d37e56b670b955f154a9f69f9
Lukas Horak <horaklukas@centrum.cz>
2021-11-30 21:34:37 +0100

    ✨ Display discover button instead of "Create *" button on landing page

--- src/app/@shell/ui/header/header.component.ts
+++ src/app/@shell/ui/header/header.component.ts
@@ -19,12 +19,13 @@ export class HeaderComponent implements OnInit {
   public enableCreateAwardProposal = false;
   public spaceSubscription$?: Subscription;
   public isMemberProfile = false;
+  public isLandingPage = false;
   constructor(
     private router: Router,
     private memberApi: MemberApi,
     private cd: ChangeDetectorRef,
     public auth: AuthService
-  ) {}
+  ) { }
 
   public ngOnInit(): void {
     this.member$.pipe(
@@ -42,27 +43,29 @@ export class HeaderComponent implements OnInit {
       }
     });
 
+    const memberRoute = `/${ROUTER_UTILS.config.member.root}/`
+    const landingPageRoute = `/${ROUTER_UTILS.config.base.home}`
+
     this.router.events.pipe(untilDestroyed(this)).subscribe((obj) => {
       if (obj instanceof NavigationStart) {
-        const previous = this.isMemberProfile;
-        if (obj.url && obj.url.startsWith(('/' + ROUTER_UTILS.config.member.root + '/'))) {
-          this.isMemberProfile = true;
-        } else {
-          this.isMemberProfile = false;
-        }
+        const previousIsMemberProfile = this.isMemberProfile;
+        const previousIsLandingPage = this.isLandingPage;
 
-        if (previous !== this.isMemberProfile) {
+        this.isMemberProfile = Boolean(obj.url?.startsWith(memberRoute))
+        this.isLandingPage = Boolean(obj.url === landingPageRoute)
+
+        if (previousIsMemberProfile !== this.isMemberProfile || previousIsLandingPage || this.isLandingPage) {
           this.cd.markForCheck();
         }
       }
-  });
+    });
   }
 
   public get isLoggedIn$(): BehaviorSubject<boolean> {
     return this.auth.isLoggedIn$;
   }
 
-  public get member$(): BehaviorSubject<Member|undefined> {
+  public get member$(): BehaviorSubject<Member | undefined> {
     return this.auth.member$;
   }
 
@@ -76,6 +79,10 @@ export class HeaderComponent implements OnInit {
 
   public get urlToNewAward(): string {
     return '/' + ROUTER_UTILS.config.award.root + '/new';
+  }
+
+  public get urlToDiscover(): string {
+    return '/' + ROUTER_UTILS.config.discover.root;
   }
 
   public goToMyProfile(): void {
