d0f2f5cc3fd167a851aa10cab0e1eac1b0017b47
Adam Kundrat <adam@kundrat.eu>
2021-11-30 08:45:19 -0800

    fixed overview

--- src/app/pages/proposal/pages/overview/overview.page.ts
+++ src/app/pages/proposal/pages/overview/overview.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { AuthService } from '@components/auth/services/auth.service';
+import { ProposalApi } from './../../../../@api/proposal.api';
+import { NotificationService } from './../../../../@core/services/notification/notification.service';
 import { DataService } from './../../services/data.service';
 
 @Component({
@@ -7,7 +11,29 @@ import { DataService } from './../../services/data.service';
   styleUrls: ['./overview.page.less']
 })
 export class OverviewPage {
-  constructor(public data: DataService) {
+  public voteControl: FormControl = new FormControl();
+  constructor(
+    private auth: AuthService,
+    private notification: NotificationService,
+    private proposalApi: ProposalApi,
+    public data: DataService
+  ) {
     // none.
+  }
+
+  public async vote(): Promise<void> {
+    if (!this.data.proposal$.value?.uid) {
+      return;
+    }
+
+    await this.auth.sign({
+      uid: this.data.proposal$.value.uid,
+      values: [this.voteControl.value]
+    }, (sc, finish) => {
+      this.notification.processRequest(this.proposalApi.vote(sc), 'Voted.', finish).subscribe((val: any) => {
+        // none.
+      });
+    });
+
   }
 }
