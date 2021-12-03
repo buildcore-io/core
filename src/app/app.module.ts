49d015f4d14c1467e9bede38206792013ca8e3f8
Adam Kundrat <adam@kundrat.eu>
2021-12-02 19:52:33 -0800

    enabled app check

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { HttpClientModule } from '@angular/common/http';
 import en from '@angular/common/locales/en';
 import fr from '@angular/common/locales/fr';
 import { LOCALE_ID, NgModule } from '@angular/core';
+import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
+import { provideAppCheck } from "@angular/fire/app-check";
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule, USE_EMULATOR as USE_FIRESTORE_EMULATOR } from '@angular/fire/compat/firestore';
 import { AngularFireFunctionsModule, USE_EMULATOR as USE_FUNCTIONS_EMULATOR } from '@angular/fire/compat/functions';
@@ -10,6 +12,7 @@ import { AngularFireStorageModule, USE_EMULATOR as USE_STORAGE_EMULATOR } from '
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IconDefinition } from '@ant-design/icons-angular';
+import { initializeAppCheck, ReCaptchaV3Provider } from '@firebase/app-check';
 import { WebShellModule } from '@shell/ft/web-shell.module';
 import { en_US as EnUs, fr_FR as FrFr, NZ_I18N as Nzi18n } from 'ng-zorro-antd/i18n';
 import { NzIconModule } from "ng-zorro-antd/icon";
@@ -19,6 +22,16 @@ registerLocaleData(en);
 registerLocaleData(fr);
 const icons: IconDefinition[] = [];
 const emulator = false;
+
+const fbConfig: any = {
+  apiKey: "AIzaSyB4fcG8rtNWAiAtSmxmK3q3JLfMvtNCGP4",
+  authDomain: "soonaverse.firebaseapp.com",
+  projectId: "soonaverse",
+  storageBucket: "soonaverse.appspot.com",
+  messagingSenderId: "502842886229",
+  appId: "1:502842886229:web:fcb7da4040fd19ba742cdc",
+  measurementId: "G-CCX9NVPPCR"
+};
 
 @NgModule({
   declarations: [WenComponent],
@@ -30,19 +43,12 @@ const emulator = false;
     BrowserAnimationsModule,
     NzIconModule.forRoot(icons),
     // Interim-Firebase.
-    AngularFireModule.initializeApp({
-      apiKey: "AIzaSyB4fcG8rtNWAiAtSmxmK3q3JLfMvtNCGP4",
-      authDomain: "soonaverse.firebaseapp.com",
-      projectId: "soonaverse",
-      storageBucket: "soonaverse.appspot.com",
-      messagingSenderId: "502842886229",
-      appId: "1:502842886229:web:fcb7da4040fd19ba742cdc",
-      measurementId: "G-CCX9NVPPCR"
+    AngularFireModule.initializeApp(fbConfig),
+    provideFirebaseApp(() => initializeApp(fbConfig)),
+    provideAppCheck(() =>  {
+      const provider = new ReCaptchaV3Provider('6LfYqHQdAAAAAI91N8xl6pc0LIUj4s9ksqj02CWm');
+      return initializeAppCheck(undefined, { provider, isTokenAutoRefreshEnabled: true });
     }),
-    // provideAppCheck(() =>  {
-    //   const provider = new ReCaptchaV3Provider('6LfYqHQdAAAAAI91N8xl6pc0LIUj4s9ksqj02CWm');
-    //   return initializeAppCheck(undefined, { provider, isTokenAutoRefreshEnabled: true });
-    // }),
     AngularFirestoreModule,
     AngularFireFunctionsModule,
     AngularFireStorageModule
