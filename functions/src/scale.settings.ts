3ceae505018773ed7cbae70b48230ad7177a10bd
Adam Kundrat <adam@kundrat.eu>
2021-12-04 13:52:40 -0800

    scaling up for the launch

--- functions/src/scale.settings.ts
+++ functions/src/scale.settings.ts
@@ -1,33 +1,36 @@
 import { WEN_FUNC } from '../interfaces/functions';
 
 export function scale(func: WEN_FUNC): number {
+  const low = 1;
+  const medium = 3;
+  const important = 6;
   const scaleSettings: any = {};
-  scaleSettings[WEN_FUNC.cMemberNotExists] = 3;
-  scaleSettings[WEN_FUNC.uMember] = 1;
+  scaleSettings[WEN_FUNC.cMemberNotExists] = important;
+  scaleSettings[WEN_FUNC.uMember] = low;
 
   // Space functions.
-  scaleSettings[WEN_FUNC.cSpace] = 1;
-  scaleSettings[WEN_FUNC.uSpace] = 1;
-  scaleSettings[WEN_FUNC.joinSpace] = 3;
-  scaleSettings[WEN_FUNC.leaveSpace] = 1;
-  scaleSettings[WEN_FUNC.addGuardianSpace] = 1;
-  scaleSettings[WEN_FUNC.removeGuardianSpace] = 1;
-  scaleSettings[WEN_FUNC.blockMemberSpace] = 1;
-  scaleSettings[WEN_FUNC.unblockMemberSpace] = 1;
-  scaleSettings[WEN_FUNC.acceptMemberSpace] = 1;
-  scaleSettings[WEN_FUNC.declineMemberSpace] = 1;
+  scaleSettings[WEN_FUNC.cSpace] = medium;
+  scaleSettings[WEN_FUNC.uSpace] = low;
+  scaleSettings[WEN_FUNC.joinSpace] = important;
+  scaleSettings[WEN_FUNC.leaveSpace] = low;
+  scaleSettings[WEN_FUNC.addGuardianSpace] = low;
+  scaleSettings[WEN_FUNC.removeGuardianSpace] = low;
+  scaleSettings[WEN_FUNC.blockMemberSpace] = low;
+  scaleSettings[WEN_FUNC.unblockMemberSpace] = low;
+  scaleSettings[WEN_FUNC.acceptMemberSpace] = medium;
+  scaleSettings[WEN_FUNC.declineMemberSpace] = low;
 
   // Award Functions
-  scaleSettings[WEN_FUNC.cAward] = 1;
-  scaleSettings[WEN_FUNC.addOwnerAward] = 1;
-  scaleSettings[WEN_FUNC.participateAward] = 1;
-  scaleSettings[WEN_FUNC.aAward] = 1;
+  scaleSettings[WEN_FUNC.cAward] = low;
+  scaleSettings[WEN_FUNC.addOwnerAward] = low;
+  scaleSettings[WEN_FUNC.participateAward] = low;
+  scaleSettings[WEN_FUNC.aAward] = low;
 
   // Proposal Functions
-  scaleSettings[WEN_FUNC.cProposal] = 1;
-  scaleSettings[WEN_FUNC.aProposal] = 1;
-  scaleSettings[WEN_FUNC.rProposal] = 1;
-  scaleSettings[WEN_FUNC.voteOnProposal] = 2;
+  scaleSettings[WEN_FUNC.cProposal] = low;
+  scaleSettings[WEN_FUNC.aProposal] = low;
+  scaleSettings[WEN_FUNC.rProposal] = low;
+  scaleSettings[WEN_FUNC.voteOnProposal] = medium;
 
-  return scaleSettings[func] || 1;
+  return scaleSettings[func] || low;
 }
