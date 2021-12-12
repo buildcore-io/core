090a6fb4e569a27d13758b71cf5b1eab2590e279
Lukas Horak <horaklukas@centrum.cz>
2021-12-12 10:51:11 +0100

    ✨ Add radio component

---
+++ src/app/components/radio/radio.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'wen-radio',
+  templateUrl: './radio.component.html',
+  styleUrls: ['./radio.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class RadioComponent {
+  @Input() value: string | boolean | number = '';
+}
