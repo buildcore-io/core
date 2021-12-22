d8d8666c66748eee663fca4b06cb9db800079196
Miriani <m.mirianashvili99@gmail.com>
2021-12-22 16:21:42 +0400

    Finish making responsive home-page

--- src/app/@core/services/device/device.service.ts
+++ src/app/@core/services/device/device.service.ts
@@ -6,10 +6,14 @@ import { BehaviorSubject, fromEvent } from 'rxjs';
 })
 export class DeviceService {
   public static MOBILE_MAX_WIDTH = 1024;
+  public isDesktop$ = new BehaviorSubject<boolean>(!this.getIsMobile());
   public isMobile$ = new BehaviorSubject<boolean>(this.getIsMobile());
 
   constructor() { 
-    fromEvent(window, 'resize').subscribe(() => this.isMobile$.next(this.getIsMobile()));
+    fromEvent(window, 'resize').subscribe(() => {
+      this.isDesktop$.next(!this.getIsMobile());
+      this.isMobile$.next(this.getIsMobile());
+    });
   }
 
   private getIsMobile(): boolean {
