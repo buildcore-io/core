b6839eb5a42cbcae8bd5211f086b468e58195192
Miriani <m.mirianashvili99@gmail.com>
2022-01-18 17:26:11 +0400

    #245 Proposal screen mobile - votes table is part of the additional tab

--- src/app/components/proposal/components/proposal-info/proposal-info.component.ts
+++ src/app/components/proposal/components/proposal-info/proposal-info.component.ts
@@ -1,6 +1,8 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { DeviceService } from '@core/services/device';
 import { DataService } from '@pages/proposal/services/data.service';
-import { Proposal, ProposalSubType } from 'functions/interfaces/models';
+import { Proposal, ProposalQuestion, ProposalSubType } from 'functions/interfaces/models';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'wen-proposal-info',
@@ -10,9 +12,18 @@ import { Proposal, ProposalSubType } from 'functions/interfaces/models';
 })
 export class ProposalInfoComponent {
   @Input() data?: DataService;
+  @Input() isGuardianWithinSpace$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
   @Input() getVotingTypeText: (subType: ProposalSubType|undefined) => string = (subType: ProposalSubType|undefined) => '';
   @Input() getCommenceDate: (proposal?: Proposal|null) => Date|null = (proposal?: Proposal|null) => null;
   @Input() getStartDate: (proposal?: Proposal|null) => Date|null = (proposal?: Proposal|null) => null;
   @Input() getEndDate: (proposal?: Proposal|null) => Date|null = (proposal?: Proposal|null) => null;
+  @Input() findAnswerText: (qs: ProposalQuestion[]|undefined, values: number[]) => string =
+    (qs: ProposalQuestion[]|undefined, values: number[]) => '';
   @Output() onExportClick = new EventEmitter<void>();
+  @Output() onApprove = new EventEmitter<void>();
+  @Output() onReject = new EventEmitter<void>();
+
+  constructor(
+    public deviceService: DeviceService
+  ) {}
 }
--- src/app/components/proposal/components/proposal-info/proposal-info.module.ts
+++ src/app/components/proposal/components/proposal-info/proposal-info.module.ts
@@ -1,7 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { IconModule } from '@components/icon/icon.module';
+import { TruncateModule } from '@core/pipes/truncate/truncate.module';
 import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzTableModule } from 'ng-zorro-antd/table';
 import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
 import { NzTypographyModule } from 'ng-zorro-antd/typography';
 import { ProposalInfoComponent } from './proposal-info.component';
@@ -17,7 +20,10 @@ import { ProposalInfoComponent } from './proposal-info.component';
     NzToolTipModule,
     IconModule,
     NzCardModule,
-    NzTypographyModule
+    NzTypographyModule,
+    RouterModule,
+    TruncateModule,
+    NzTableModule
   ],
   exports: [
     ProposalInfoComponent
