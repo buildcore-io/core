abe213ceb2d36c716fdb9b22b250a5f3c3ddda49
Miriani <m.mirianashvili99@gmail.com>
2022-01-31 17:33:50 +0400

    #189

--- src/app/components/space/components/space-new-alliance/space-new-alliance.component.ts
+++ src/app/components/space/components/space-new-alliance/space-new-alliance.component.ts
@@ -3,6 +3,7 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { SelectBoxOption, SelectBoxSizes } from '@components/select-box/select-box.component';
 import { AvatarService } from '@core/services/avatar';
+import { DataService } from '@pages/space/services/data.service';
 import { Space } from 'functions/interfaces/models';
 
 @Component({
@@ -14,11 +15,16 @@ import { Space } from 'functions/interfaces/models';
 export class SpaceNewAllianceComponent {
   @Input() 
   public set spaces(value: Space[]) {
-    this.spaceOptions = value.map(space => ({
-      label: space.name || '',
-      value: space.uid,
-      img: this.avatarService.getAvatarSize(space.avatarUrl)
-    }));
+    const currentSpaceUid = this.data.space$.getValue()?.uid;
+    
+    this.spaceOptions =
+      value
+        .filter(space => space.uid !== currentSpaceUid)
+        .map(space => ({
+          label: space.name || '',
+          value: space.uid,
+          img: this.avatarService.getAvatarSize(space.avatarUrl)
+        }));
   }
   @Input() spaceAllianceControl: FormControl = new FormControl('');
   @Input() reputationWeightControl: FormControl = new FormControl(null);
@@ -32,7 +38,8 @@ export class SpaceNewAllianceComponent {
   private _spaces: Space[] = [];
 
   constructor(
-    public avatarService: AvatarService
+    public avatarService: AvatarService,
+    private data: DataService
   ) {}
   
   public trackByUid(index: number, item: any): number {
