a9e96e8703f9cc704f534750a907e4cbcdd0df6b
Adam <adam@kundrat.eu>
2022-02-04 17:26:01 -0800

    new transaction types

--- functions/interfaces/models/base.ts
+++ functions/interfaces/models/base.ts
@@ -45,6 +45,7 @@ export const enum AWARD_COL {
 }
 
 export type EthAddress = string;
+export type IotaAddress = string;
 export type IpfsCid = string;
 
 export interface Base {
--- functions/interfaces/models/transaction.ts
+++ functions/interfaces/models/transaction.ts
@@ -1,8 +1,17 @@
-import { BaseRecord, FileMetedata } from './base';
+import { BaseRecord, EthAddress, FileMetedata, IotaAddress } from './base';
 export enum TransactionType {
   BADGE = "BADGE",
   VOTE = "VOTE",
-  PLEDGE = "PLEDGE"
+  PLEDGE = "PLEDGE",
+  ORDER = "ORDER",
+  PAYMENT = "PAYMENT",
+  CREDIT = "CREDIT"
+}
+
+export enum TransactionOrderType {
+  NFT_PURCHASE = "NFT_PURCHASE",
+  SPACE_ADDRESS_VALIDATION = "SPACE_ADDRESS_VALIDATION",
+  MEMBER_ADDRESS_VALIDATION = "MEMBER_ADDRESS_VALIDATION",
 }
 
 export interface VoteTransaction {
@@ -18,8 +27,40 @@ export interface BadgeTransaction {
   xp: number;
 }
 
+export interface OrderTransaction {
+  amount: number;
+  targetAddress: IotaAddress;
+  type: TransactionOrderType;
+  linkedTransactions: EthAddress[];
+}
+
+export interface PaymentTransaction {
+  amount: number;
+  targetAddress: IotaAddress;
+  reconciled: boolean;
+  chainReference: string;
+  sourceTransaction: OrderTransaction;
+}
+
+export interface BillTransaction {
+  amount: number;
+  targetAddress: IotaAddress;
+  reconciled: boolean;
+  chainReference: string;
+  sourceTransaction: OrderTransaction;
+}
+
+export interface CreditTransaction {
+  amount: number;
+  targetAddress: IotaAddress;
+  reconciled: boolean;
+  chainReference: string;
+  sourceTransaction: OrderTransaction;
+}
+
 export interface Transaction extends BaseRecord {
   type: TransactionType;
-  member?: string;
-  payload: any; // VoteTransaction|BadgeTransaction;
+  member?: EthAddress;
+  space?: EthAddress;
+  payload: any; // VoteTransaction|BadgeTransaction|OrderTransaction|PaymentTransaction|BillTransaction|CreditTransaction;
 }
