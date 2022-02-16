ac28e795207fb3a7857a84da67815595221ee685
Adam <adam@kundrat.eu>
2022-02-15 23:36:02 -0800

    go to collection instead

--- src/app/pages/nft/pages/new/single/single.page.ts
+++ src/app/pages/nft/pages/new/single/single.page.ts
@@ -251,7 +251,7 @@ export class SinglePage implements OnInit {
 
     await this.auth.sign(this.formatSubmitData({...this.nftForm.value}), (sc, finish) => {
       this.notification.processRequest(this.nftApi.create(sc), 'Created.', finish).subscribe((val: any) => {
-        this.router.navigate([ROUTER_UTILS.config.nft.root, val?.uid]);
+        this.router.navigate([ROUTER_UTILS.config.collection.root]);
       });
     });
   }
