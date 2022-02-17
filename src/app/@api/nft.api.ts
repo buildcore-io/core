590ddb8efce09be7571a220417a593fd5d42d9c1
Adam <adam@kundrat.eu>
2022-02-16 23:48:11 -0800

    minor tweaks

--- src/app/@api/nft.api.ts
+++ src/app/@api/nft.api.ts
@@ -44,6 +44,12 @@ export class NftApi extends BaseApi<Nft> {
     });
   }
 
+  public positionInCollection(collection: string, lastValue?: any, search?: string, def = DEFAULT_LIST_SIZE): Observable<Nft[]> {
+    return this._query(this.collection, 'position', 'asc', lastValue, search, def, (ref: any) => {
+      return ref.where('collection', '==', collection).where('hidden', '==', false);
+    });
+  }
+
   public recentlyChangedCollection(collection: string, lastValue?: any, search?: string, def = DEFAULT_LIST_SIZE): Observable<Nft[]> {
     return this._query(this.collection, 'updatedOn', 'desc', lastValue, search, def, (ref: any) => {
       return ref.where('collection', '==', collection).where('hidden', '==', false);
