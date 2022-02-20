46fcf6c4249728ba20fc559b5838e9df8d0d1cec
Josef Frnka <josef@infinetcloud.com>
2022-02-21 08:42:21 +1100

    timeline fix, copy btn fix

--- src/app/pages/nft/pages/nft/nft.page.ts
+++ src/app/pages/nft/pages/nft/nft.page.ts
@@ -146,6 +146,7 @@ export class NFTPage implements OnInit, OnDestroy {
   public copy(): void {
     if (!this.isCopied) {
       copyToClipboard(window.location.href);
+      this.isCopied = true;
       setTimeout(() => {
         this.isCopied = false;
         this.cd.markForCheck();
