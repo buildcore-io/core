ecd447f47b2f5da57db250982a14d402a2d995c8
Miriani <m.mirianashvili99@gmail.com>
2022-02-21 19:39:14 +0400

    #300

--- src/app/pages/collection/pages/upsert/upsert.page.ts
+++ src/app/pages/collection/pages/upsert/upsert.page.ts
@@ -215,6 +215,7 @@ export class UpsertPage implements OnInit, OnDestroy {
               this.selectedAccessControl.disable();
               this.accessAwardsControl.disable();
               this.priceControl.disable();
+              this.unitControl.disable();
               this.availableFromControl.disable();
               this.typeControl.disable();
               this.categoryControl.disable();
