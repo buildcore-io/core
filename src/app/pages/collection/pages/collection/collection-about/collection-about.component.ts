ed040ab9db96b8ade57ba99f652c4aefcf0cb5af
Adam <adam@kundrat.eu>
2022-03-11 23:18:32 -0800

    short url

--- src/app/pages/collection/pages/collection/collection-about/collection-about.component.ts
+++ src/app/pages/collection/pages/collection/collection-about/collection-about.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { DeviceService } from '@core/services/device';
-import { CollectionAccess, DiscountLine } from '@functions/interfaces/models';
+import { Collection, CollectionAccess, DiscountLine } from '@functions/interfaces/models';
 import { DataService } from '../../../services/data.service';
 
 @Component({
@@ -51,8 +51,9 @@ export class CollectionAboutComponent {
     });
   }
 
-  public getShareUrl(): string {
+  public getShareUrl(col?: Collection | null): string {
     const text = $localize`Check out collection`;
-    return 'http://twitter.com/share?text= ' + text + ' &url=' + window.location.href + '&hashtags=soonaverse';
+    const url: string = (col?.wenUrlShort || col?.wenUrl || window.location.href);
+    return 'http://twitter.com/share?text= ' + text + ' &url=' + url + '&hashtags=soonaverse';
   }
 }
