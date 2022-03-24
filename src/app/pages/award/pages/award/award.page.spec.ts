ce3b910442a80d7f262465a092b730aa5d7c00de
Adam <adam@kundrat.eu>
2022-03-24 15:40:34 -0700

    added limited edition

--- src/app/pages/award/pages/award/award.page.spec.ts
+++ src/app/pages/award/pages/award/award.page.spec.ts
@@ -1,4 +1,5 @@
 import { IconModule } from '@components/icon/icon.module';
+import { ShareModule } from '@components/share/share.module';
 import { IpfsAvatarModule } from "@core/pipes/ipfs-avatar/ipfs-avatar.module";
 import { createRoutingFactory, Spectator } from '@ngneat/spectator/jest';
 import { DataService } from "@pages/award/services/data.service";
@@ -9,10 +10,10 @@ import { AwardApi } from './../../../../@api/award.api';
 import { IpfsBadgeModule } from './../../../../@core/pipes/ipfs-badge/ipfs-badge.module';
 import { NavigationService } from './../../../../@core/services/navigation/navigation.service';
 import { AwardAwardsModule } from './../../../../components/award/components/award-awards/award-awards.module';
-import { AwardInfoModule } from './award-info/award-info.module';
 import { AwardStatusModule } from './../../../../components/award/components/award-status/award-status.module';
 import { DrawerToggleModule } from './../../../../components/drawer-toggle/drawer-toggle.module';
 import { TabsModule } from './../../../../components/tabs/tabs.module';
+import { AwardInfoModule } from './award-info/award-info.module';
 import { AwardPage } from './award.page';
 
 
@@ -25,7 +26,7 @@ describe('AwardPage', () => {
         return new Observable();
       }
     })],
-    imports: [TabsModule, IconModule, IpfsBadgeModule, IpfsAvatarModule, AwardStatusModule, LayoutModule, DrawerToggleModule, AwardInfoModule, AwardAwardsModule ]
+    imports: [TabsModule, IconModule, IpfsBadgeModule, IpfsAvatarModule, AwardStatusModule, LayoutModule, DrawerToggleModule, AwardInfoModule, AwardAwardsModule, ShareModule ]
   });
 
   beforeEach(() => {
