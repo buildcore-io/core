a5f0949398949e355824623ada28bcf9d2c583da
Miriani <m.mirianashvili99@gmail.com>
2022-03-25 17:56:16 +0400

    #468 Access tags for Collection/NFT cards

--- src/app/components/nft/components/nft-card/nft-card.component.ts
+++ src/app/components/nft/components/nft-card/nft-card.component.ts
@@ -8,7 +8,7 @@ import { getItem, StorageItem } from '@core/utils';
 import { ROUTER_UTILS } from '@core/utils/router.utils';
 import { UnitsHelper } from '@core/utils/units-helper';
 import { MIN_AMOUNT_TO_TRANSFER } from '@functions/interfaces/config';
-import { Collection, CollectionType, Member } from '@functions/interfaces/models';
+import { Collection, CollectionAccess, CollectionType, Member } from '@functions/interfaces/models';
 import { FILE_SIZES } from '@functions/interfaces/models/base';
 import { Nft } from '@functions/interfaces/models/nft';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
@@ -136,6 +136,10 @@ export class NftCardComponent {
 
   public get filesizes(): typeof FILE_SIZES {
     return FILE_SIZES;
+  }
+
+  public get targetAccess(): typeof CollectionAccess {
+    return CollectionAccess;
   }
 
   public getBadgeProperties(): { label: string; className: string} {
