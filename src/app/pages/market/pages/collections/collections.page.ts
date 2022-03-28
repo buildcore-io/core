dfe39f4f26806c7136905db88f1d2d7c8bfcc251
Adam <adam@kundrat.eu>
2022-03-27 17:12:02 -0700

    fixed filtering on marketplace collections

--- src/app/pages/market/pages/collections/collections.page.ts
+++ src/app/pages/market/pages/collections/collections.page.ts
@@ -14,8 +14,7 @@ import { BehaviorSubject, map, Observable, skip, Subscription } from 'rxjs';
 
 export enum HOT_TAGS {
   ALL = 'All',
-  OPEN_SALE_ONLY = 'Open Sale Only',
-  MEMBERS_ONLY = 'Members Only',
+  ACCESS = 'ACCESS',
   SPACE = 'SPACE',
   CATEGORY = 'CATEGORY'
 }
@@ -37,7 +36,6 @@ export class CollectionsPage implements OnInit, OnDestroy {
   public categoryControl: FormControl;
   public collections$: BehaviorSubject<Collection[]|undefined> = new BehaviorSubject<Collection[]|undefined>(undefined);
   public accessControl: FormControl;
-  public hotTags: string[] = [HOT_TAGS.ALL, HOT_TAGS.OPEN_SALE_ONLY, HOT_TAGS.MEMBERS_ONLY];
   public categories: string[] = [AddedCategories.ALL, Categories.ABSTRACT, Categories.ANIMATION, Categories.ART,
                                 Categories.COLLECTIBLE, Categories.GENERATIVE, Categories.INTERACTIVE, Categories.PFP,
                                 Categories.PHOTOGRAPHY, Categories.PIXELART, Categories.SINGLE, Categories.THREE_D ];
@@ -85,6 +83,10 @@ export class CollectionsPage implements OnInit, OnDestroy {
         this.spaceControl.setValue(DEFAULT_SPACE.value, { emitEvent: false })
       }
 
+      if (o.indexOf(HOT_TAGS.ACCESS) === -1 && this.accessControl.value !== this.accessTypes.ALL) {
+        this.accessControl.setValue(this.accessTypes.ALL, { emitEvent: false })
+      }
+
       if (this.filter.search$.value && this.filter.search$.value.length > 0) {
         this.listen(this.filter.search$.value);
       } else {
@@ -93,17 +95,25 @@ export class CollectionsPage implements OnInit, OnDestroy {
     });
 
     this.spaceControl.valueChanges.pipe(untilDestroyed(this)).subscribe((obj) => {
-      if (obj && obj !== DEFAULT_SPACE.value && this.selectedTags$.value.indexOf(HOT_TAGS.SPACE)) {
+      if (obj && obj !== DEFAULT_SPACE.value) {
         this.selectedTags$.next([HOT_TAGS.SPACE]);
-      } else if (this.selectedTags$.value.indexOf(HOT_TAGS.SPACE) === -1) {
+      } else if (obj === DEFAULT_SPACE.value) {
         this.selectedTags$.next([HOT_TAGS.ALL]);
       }
     });
 
     this.categoryControl.valueChanges.pipe(untilDestroyed(this)).subscribe((obj) => {
-      if (obj && obj !== AddedCategories.ALL && this.selectedTags$.value.indexOf(HOT_TAGS.CATEGORY)) {
+      if (obj && obj !== AddedCategories.ALL) {
         this.selectedTags$.next([HOT_TAGS.CATEGORY]);
-      } else if (this.selectedTags$.value.indexOf(HOT_TAGS.CATEGORY) === -1) {
+      } else if (obj === AddedCategories.ALL) {
+        this.selectedTags$.next([HOT_TAGS.ALL]);
+      }
+    });
+
+    this.accessControl.valueChanges.pipe(untilDestroyed(this)).subscribe((obj) => {
+      if (obj > -1 && obj !== this.accessTypes.ALL) {
+        this.selectedTags$.next([HOT_TAGS.ACCESS]);
+      } else if (obj === this.accessTypes.ALL) {
         this.selectedTags$.next([HOT_TAGS.ALL]);
       }
     });
@@ -116,10 +126,8 @@ export class CollectionsPage implements OnInit, OnDestroy {
 
   public getHandler(last?: any, search?: string): Observable<Collection[]> {
     if (this.filter.selectedSort$.value === SortOptions.PRICE_LOW) {
-      if (this.selectedTags$.value[0] === HOT_TAGS.OPEN_SALE_ONLY) {
-        return this.collectionApi.lowToHighAccess(CollectionAccess.OPEN, last, search);
-      } else if (this.selectedTags$.value[0] === HOT_TAGS.MEMBERS_ONLY) {
-        return this.collectionApi.lowToHighAccess(CollectionAccess.MEMBERS_ONLY, last, search);
+      if (this.selectedTags$.value[0] === HOT_TAGS.ACCESS) {
+        return this.collectionApi.lowToHighAccess(this.accessControl.value, last, search);
       } else if (this.selectedTags$.value[0] === HOT_TAGS.SPACE) {
         return this.collectionApi.lowToHighSpace(this.spaceControl.value, last, search);
       } else if (this.selectedTags$.value[0] === HOT_TAGS.CATEGORY) {
@@ -128,10 +136,8 @@ export class CollectionsPage implements OnInit, OnDestroy {
         return this.collectionApi.lowToHigh(last, search);
       }
     } else if (this.filter.selectedSort$.value === SortOptions.RECENT) {
-      if (this.selectedTags$.value[0] === HOT_TAGS.OPEN_SALE_ONLY) {
-        return this.collectionApi.topAccess(CollectionAccess.OPEN, last, search);
-      } else if (this.selectedTags$.value[0] === HOT_TAGS.MEMBERS_ONLY) {
-        return this.collectionApi.topAccess(CollectionAccess.MEMBERS_ONLY, last, search);
+      if (this.selectedTags$.value[0] === HOT_TAGS.ACCESS) {
+        return this.collectionApi.topAccess(this.accessControl.value, last, search);
       } else if (this.selectedTags$.value[0] === HOT_TAGS.SPACE) {
         return this.collectionApi.topSpace(this.spaceControl.value, last, search);
       } else if (this.selectedTags$.value[0] === HOT_TAGS.CATEGORY) {
@@ -140,10 +146,8 @@ export class CollectionsPage implements OnInit, OnDestroy {
         return this.collectionApi.topApproved(last, search);
       }
     } else {
-      if (this.selectedTags$.value[0] === HOT_TAGS.OPEN_SALE_ONLY) {
-        return this.collectionApi.highToLowAccess(CollectionAccess.OPEN, last, search);
-      } else if (this.selectedTags$.value[0] === HOT_TAGS.MEMBERS_ONLY) {
-        return this.collectionApi.highToLowAccess(CollectionAccess.MEMBERS_ONLY, last, search);
+      if (this.selectedTags$.value[0] === HOT_TAGS.ACCESS) {
+        return this.collectionApi.highToLowAccess(this.accessControl.value, last, search);
       } else if (this.selectedTags$.value[0] === HOT_TAGS.SPACE) {
         return this.collectionApi.highToLowSpace(this.spaceControl.value, last, search);
       } else if (this.selectedTags$.value[0] === HOT_TAGS.CATEGORY) {
@@ -152,10 +156,6 @@ export class CollectionsPage implements OnInit, OnDestroy {
         return this.collectionApi.highToLow(last, search);
       }
     }
-  }
-
-  public handleChange(tag: string): void {
-    this.selectedTags$.next([tag]);
   }
 
   public getSpaceListOptions(list?: Space[] | null): SelectSpaceOption[] {
@@ -212,7 +212,7 @@ export class CollectionsPage implements OnInit, OnDestroy {
       return (!this.dataStore[this.dataStore.length - 1] || this.dataStore[this.dataStore.length - 1]?.length < DEFAULT_LIST_SIZE);
     }));
   }
-  
+
   public get accessTypes(): typeof CollectionAccess & { ALL: string } {
     return { ...CollectionAccess, ALL: 'ALL' };
   }
