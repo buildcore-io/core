31ad8b9d3333b5ed0a68a6c6e0ee90c5762c2386
Miriani <m.mirianashvili99@gmail.com>
2022-05-18 15:26:07 +0400

    Implement airdrops and refund functionality

--- src/app/pages/token/pages/airdrops/airdrops.page.spec.ts
+++ src/app/pages/token/pages/airdrops/airdrops.page.spec.ts
@@ -1,4 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { MemberApi } from '@api/member.api';
+import { TokenApi } from '@api/token.api';
+import { AuthService } from '@components/auth/services/auth.service';
+import { NotificationService } from '@core/services/notification';
+import { MockProvider } from 'ng-mocks';
 import { AirdropsPage } from './airdrops.page';
 
 
@@ -8,7 +14,14 @@ describe('AirdropsPage', () => {
 
   beforeEach(async() => {
     await TestBed.configureTestingModule({
-      declarations: [ AirdropsPage ]
+      declarations: [ AirdropsPage ],
+      providers: [
+        MockProvider(AuthService),
+        MockProvider(MemberApi),
+        MockProvider(NotificationService),
+        MockProvider(Router),
+        MockProvider(TokenApi)
+      ]
     })
       .compileComponents();
   });
