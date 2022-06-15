85942638894b1b47d64cec4ddb075b329fd6b8bc
adam_unchained <adam@unchained.work>
2022-06-14 21:31:48 -0700

    fixed roll over script

--- functions/scripts/dbUpgrades/rollValidatedAddress.ts
+++ functions/scripts/dbUpgrades/rollValidatedAddress.ts
@@ -1,8 +1,8 @@
-import { Network } from '@functions/interfaces/models';
-import { COL } from '@functions/interfaces/models/base';
 import { cert, initializeApp } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 import { last } from 'lodash';
+import { Network } from '../../interfaces/models';
+import { COL } from "../../interfaces/models/base";
 import serviceAccount from '../serviceAccountKeyTest.json';
 
 initializeApp({
@@ -14,14 +14,24 @@ const db = getFirestore();
 export const rolValidatedAddress = async (collection: COL) => {
   let lastDoc: any
   do {
-    const snap = await db.collection(collection).orderBy('createdOn').startAfter(lastDoc).limit(500).get()
+    let snap: any = db.collection(collection).orderBy('createdOn');
+    if (lastDoc) {
+      snap = snap.startAfter(lastDoc);
+    }
+    snap = await snap.limit(500).get();
+
     lastDoc = last(snap.docs)
-    const promises = snap.docs.map(doc => {
+    const promises = snap.docs.map((doc: any) => {
       const validatedAddress = doc.data()?.validatedAddress
       if (validatedAddress && typeof validatedAddress === 'string') {
+        console.log({ validatedAddress: { [Network.IOTA]: doc.data()?.validatedAddress } });
         doc.ref.update({ validatedAddress: { [Network.IOTA]: doc.data()?.validatedAddress } })
       }
     })
     await Promise.all(promises)
   } while (lastDoc !== undefined)
 }
+
+// Records.
+rolValidatedAddress(COL.MEMBER);
+rolValidatedAddress(COL.SPACE);
