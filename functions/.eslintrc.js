27d27a7930ed2e402178efc002b6f533972e86ee
Boldizsar Mezei <boldizsar@unchained@work.com>
2022-07-28 03:03:23 +0200

    Test

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
     '/test/**/*',
     '/test-online/**/*',
     '/test-load/**/*',
+    '/workflow.build.js'
   ],
   plugins: ['@typescript-eslint', 'import'],
   rules: {
---
+++ functions/test/workflow.spec.ts
@@ -0,0 +1,18 @@
+import fs from 'fs';
+import glob from 'glob';
+
+describe('Workflow test', () => {
+
+  it('Test if workflow contains all files', async () => {
+    const buffer = fs.readFileSync('../.github/workflows/functions-unit-tests_emulator.yml');
+    const workflowTxt = buffer.toString();
+
+    const testFileNames = glob.sync(`./test/**/*.spec.ts`)
+    for (const testFileName of testFileNames) {
+      const fileName = testFileName.replace(/\./g, "-").replace(/\//g, "-")
+      if (!workflowTxt.includes(fileName)) {
+        throw Error(`functions-unit-tests_emulator.yml misses the following file: ${testFileName}. Pls run node workflow.build.js`)
+      }
+    }
+  })
+})
