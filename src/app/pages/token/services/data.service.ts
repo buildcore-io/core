4e504331653ba59bbfcff1281fd3d9c05e820c85
adam_unchained <adam@unchained.work>
2022-09-01 20:20:33 -0700

    only show tokens that bought

--- src/app/pages/token/services/data.service.ts
+++ src/app/pages/token/services/data.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { Space } from "@functions/interfaces/models";
 import { Token, TokenDistribution } from "@functions/interfaces/models/token";
-import { BehaviorSubject } from "rxjs";
+import { BehaviorSubject, map } from "rxjs";
 
 @Injectable({
   providedIn: 'any'
@@ -10,8 +10,18 @@ export class DataService {
   public token$: BehaviorSubject<Token | undefined> = new BehaviorSubject<Token | undefined>(undefined);
   public space$: BehaviorSubject<Space | undefined> = new BehaviorSubject<Space | undefined>(undefined);
   public distributions$: BehaviorSubject<TokenDistribution[] | undefined> = new BehaviorSubject<TokenDistribution[] | undefined>(undefined);
+  public distributionsBought$: BehaviorSubject<TokenDistribution[] | undefined> = new BehaviorSubject<TokenDistribution[] | undefined>(undefined);
   public memberDistribution$?: BehaviorSubject<TokenDistribution | undefined> = new BehaviorSubject<TokenDistribution | undefined>(undefined);
 
+  constructor() {
+    this.distributionsBought$.pipe(
+      map((dis: TokenDistribution[] | undefined) => {
+        return dis?.filter((d: TokenDistribution) => {
+          return d && (d.totalDeposit || d.totalBought);
+        });
+      })
+    );
+  }
   public resetSubjects(): void {
     // Clean up all streams.
     this.token$.next(undefined);
