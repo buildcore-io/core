2822e9bc7cd2b6107520c2933e58c9f7ece8d9c9
Gundavri <51381939+Gundavri@users.noreply.github.com>
2022-09-19 10:59:09 -0700

    remove Gi/Mi in inputs (#1505)
    remove Gi/Mi in inputs (#1505)

--- src/app/components/nft/components/nft-sale/nft-sale-fixed-price/nft-sale-fixed-price.component.ts
+++ src/app/components/nft/components/nft-sale/nft-sale-fixed-price/nft-sale-fixed-price.component.ts
@@ -1,16 +1,16 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AlgoliaService } from '@components/algolia/services/algolia.service';
-import { Units, UnitsService } from '@core/services/units';
+import { UnitsService } from '@core/services/units';
 import { MAX_IOTA_AMOUNT, MIN_IOTA_AMOUNT } from '@functions/interfaces/config';
 import { Member } from '@functions/interfaces/models';
 import { COL } from '@functions/interfaces/models/base';
-import { Nft, NftAccess, PRICE_UNITS } from '@functions/interfaces/models/nft';
+import { Nft, NftAccess } from '@functions/interfaces/models/nft';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { HelperService } from '@pages/nft/services/helper.service';
 import dayjs from 'dayjs';
 import { NzSelectOptionInterface } from 'ng-zorro-antd/select';
-import { BehaviorSubject, from, merge, Subscription } from 'rxjs';
+import { BehaviorSubject, from, Subscription } from 'rxjs';
 import { SaleType, UpdateEvent } from '../nft-sale.component';
 
 @UntilDestroy()
@@ -29,13 +29,7 @@ export class NftSaleFixedPriceComponent implements OnInit, OnDestroy {
       this.selectedAccessControl.setValue(this._nft.saleAccess || NftAccess.OPEN);
       this.buyerControl.setValue(this._nft.saleAccessMembers || []);
       if (this._nft.availablePrice) {
-        if (this._nft.availablePrice >= 1000 * 1000 * 1000) {
-          this.priceControl.setValue(this._nft.availablePrice / 1000 / 1000 / 1000);
-          this.unitControl.setValue(<Units>'Gi');
-        } else {
-          this.priceControl.setValue(this._nft.availablePrice / 1000 / 1000);
-          this.unitControl.setValue(<Units>'Mi');
-        }
+        this.priceControl.setValue(this._nft.availablePrice / 1000 / 1000);
       }
     }
   }
@@ -44,8 +38,7 @@ export class NftSaleFixedPriceComponent implements OnInit, OnDestroy {
   }
   @Output() public wenOnUpdate = new EventEmitter<UpdateEvent>();
   public form: FormGroup;
-  public priceControl: FormControl = new FormControl('', [Validators.required, Validators.min(0), Validators.max(1000)]);
-  public unitControl: FormControl = new FormControl(PRICE_UNITS[0], Validators.required);
+  public priceControl: FormControl = new FormControl('', [Validators.required, Validators.min(MIN_IOTA_AMOUNT / 1000 / 1000), Validators.max(MAX_IOTA_AMOUNT / 1000 / 1000)]);
   public availableFromControl: FormControl = new FormControl('', Validators.required);
   public selectedAccessControl: FormControl = new FormControl(NftAccess.OPEN, Validators.required);
   public buyerControl: FormControl = new FormControl('');
@@ -62,7 +55,6 @@ export class NftSaleFixedPriceComponent implements OnInit, OnDestroy {
   ) {
     this.form = new FormGroup({
       price: this.priceControl,
-      unit: this.unitControl,
       availableFrom: this.availableFromControl,
       selectedAccess: this.selectedAccessControl,
       buyer: this.buyerControl
@@ -70,14 +62,6 @@ export class NftSaleFixedPriceComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
-    merge(this.unitControl.valueChanges, this.priceControl.valueChanges)
-      .pipe(untilDestroyed(this))
-      .subscribe(() => {
-        const value = this.getRawPrice(Number(this.priceControl.value), <Units> this.unitControl.value);
-        const errors = value >= MIN_IOTA_AMOUNT && value <= MAX_IOTA_AMOUNT ? null : { price: { valid: false } };
-        this.priceControl.setErrors(errors);
-      });
-
     this.selectedAccessControl.valueChanges.pipe(untilDestroyed(this))
       .subscribe(() => {
         switch (this.selectedAccessControl.value) {
@@ -111,14 +95,6 @@ export class NftSaleFixedPriceComponent implements OnInit, OnDestroy {
       });
   }
 
-  private getRawPrice(price: number, unit: Units): number {
-    return price * (unit === 'Gi' ? 1000 * 1000 * 1000 : 1000 * 1000);
-  }
-
-  public get priceUnits(): Units[] {
-    return PRICE_UNITS;
-  }
-
   public disabledStartDate(startValue: Date): boolean {
     if (dayjs(startValue).isBefore(dayjs(), 'days')) {
       return true;
@@ -141,7 +117,7 @@ export class NftSaleFixedPriceComponent implements OnInit, OnDestroy {
     this.wenOnUpdate.next({
       type: SaleType.FIXED_PRICE,
       availableFrom: this.availableFromControl.value,
-      price: this.getRawPrice(this.priceControl.value, this.unitControl.value),
+      price: this.priceControl.value * 1000 * 1000,
       access: this.selectedAccessControl.value,
       accessMembers: this.buyerControl.value
     });
