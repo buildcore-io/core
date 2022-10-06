fc376d5a94adf992da94b484ad208633b5137c5b
Boldizsar Mezei <boldizsar@unchained.work>
2022-10-06 09:28:50 +0200

    Use cache and parallel execution in actions
    Test commit
    
    Test commit
    
    Fixes
    
    Fixes
    
    Fixes
    
    Fixes
    
    fix
    
    Fixes
    
    fixes
    
    Fixes
    
    Fixes
    
    Fixes
    
    Fixes
    
    Fixes
    
    Fixes
    
    fixes
    
    Fixes
    
    Fixes
    
    asd
    
    Fixes
    
    Fixes
    
    Fixes
    
    Fixes
    
    Fixes
    
    Fixes
    
    Fixes
    
    fix
    
    fix
    
    fixes
    
    fixes
    
    Fixes
    
    Fix
    
    Fixes
    
    Fixes
    
    Fixes
    
    Fixes
    
    Fixes

--- functions/test-tangle/workflow-online.build.js
+++ functions/test-tangle/workflow-online.build.js
@@ -9,38 +9,82 @@ function getJobForFile(filePath) {
   const fileName = filePath
     .replaceAll('./test-tangle/', '')
     .replaceAll('/', '_')
-    .replaceAll('.', '-')
+    .replaceAll('.', '-');
   fs.appendFileSync(outputFile, `  ${fileName}:\n`);
+  fs.appendFileSync(outputFile, `    needs: npm-intstall\n`);
   fs.appendFileSync(outputFile, `    runs-on: ubuntu-latest\n`);
-  fs.appendFileSync(outputFile, `    timeout-minutes: 60\n`);
+  fs.appendFileSync(outputFile, `    timeout-minutes: 30\n`);
   fs.appendFileSync(outputFile, `    defaults:\n`);
   fs.appendFileSync(outputFile, `      run:\n`);
   fs.appendFileSync(outputFile, `        working-directory: functions\n`);
+  fs.appendFileSync(outputFile, `    env:\n`);
+  fs.appendFileSync(
+    outputFile,
+    `      FIREBASE_TOKEN: \${{ secrets.FIREBASE_DEV_TOKEN }}\n`,
+  );
   fs.appendFileSync(outputFile, `    steps:\n`);
   fs.appendFileSync(outputFile, `      - uses: actions/checkout@v2\n`);
   fs.appendFileSync(outputFile, `      - uses: actions/setup-node@v1\n`);
   fs.appendFileSync(outputFile, `        with:\n`);
   fs.appendFileSync(outputFile, `          node-version: '16'\n`);
-  fs.appendFileSync(outputFile, `      - run: npm install\n`);
+  fs.appendFileSync(outputFile, `      - uses: actions/cache@v3\n`);
+  fs.appendFileSync(outputFile, `        with:\n`);
+  fs.appendFileSync(outputFile, `          path: '**/node_modules'\n`);
+  fs.appendFileSync(
+    outputFile,
+    `          key: \${{ runner.os }}-modules-\${{ hashFiles('**/package.json') }}\n`,
+  );
+  fs.appendFileSync(outputFile, `      - run: npm install -g firebase-tools\n`);
   fs.appendFileSync(outputFile, `      - run: npm run build\n`);
   fs.appendFileSync(
     outputFile,
-    `      - run: npm run test-tangle-online -- --findRelatedTests ${filePath}\n\n`,
+    `      - run: ./node_modules/.bin/firebase use dev\n`,
+  );
+  fs.appendFileSync(
+    outputFile,
+    `      - run: ./node_modules/.bin/firebase emulators:exec "npm run test-tangle-online -- --findRelatedTests ${filePath}" --project dev\n\n`,
   );
 }
 
 try {
   fs.writeFileSync(
     outputFile,
-    'name: Tangle - Online - Functions Emulated Unit Tests\n',
+    'name: Tangle - Functions Emulated Unit Tests\n\n',
   );
-  fs.appendFileSync(outputFile, 'on:\n')
-  fs.appendFileSync(outputFile, '  workflow_run:\n')
-  fs.appendFileSync(outputFile, '    workflows: ["Firebase Deploy DEV"]\n')
-  fs.appendFileSync(outputFile, '    types: [completed]\n')
-  fs.appendFileSync(outputFile, '    branches:\n')
-  fs.appendFileSync(outputFile, '      - "develop"\n')
+  fs.appendFileSync(outputFile, 'on:\n');
+  fs.appendFileSync(outputFile, '  workflow_run:\n');
+  fs.appendFileSync(outputFile, '    workflows: ["Firebase Deploy DEV"]\n');
+  fs.appendFileSync(outputFile, '    types: [completed]\n');
+  fs.appendFileSync(outputFile, '    branches:\n');
+  fs.appendFileSync(outputFile, '      - "develop"\n');
+
   fs.appendFileSync(outputFile, 'jobs:\n');
+  fs.appendFileSync(outputFile, '  npm-intstall:\n');
+  fs.appendFileSync(outputFile, '    runs-on: ubuntu-latest\n');
+  fs.appendFileSync(outputFile, '    timeout-minutes: 10\n');
+  fs.appendFileSync(outputFile, '    defaults:\n');
+  fs.appendFileSync(outputFile, '      run:\n');
+  fs.appendFileSync(outputFile, '        working-directory: functions\n');
+  fs.appendFileSync(outputFile, '    steps:\n');
+  fs.appendFileSync(outputFile, '      - uses: actions/checkout@v3\n');
+  fs.appendFileSync(outputFile, '      - uses: actions/setup-node@v3\n');
+  fs.appendFileSync(outputFile, '        with:\n');
+  fs.appendFileSync(outputFile, '          node-version: 16\n');
+  fs.appendFileSync(outputFile, '      - uses: actions/cache@v3\n');
+  fs.appendFileSync(outputFile, '        id: cache\n');
+  fs.appendFileSync(outputFile, '        with:\n');
+  fs.appendFileSync(outputFile, "          path: '**/node_modules'\n");
+  fs.appendFileSync(
+    outputFile,
+    "          key: ${{ runner.os }}-modules-${{ hashFiles('**/package.json') }}\n",
+  );
+  fs.appendFileSync(outputFile, '      - name: Install Dependencies\n');
+  fs.appendFileSync(
+    outputFile,
+    "        if: steps.cache.outputs.cache-hit != 'true'\n",
+  );
+  fs.appendFileSync(outputFile, '        run: npm install\n\n');
+
   glob.sync(`./test-tangle/**/*.spec.ts`).forEach(getJobForFile);
 } catch (e) {
   console.error(errorMsg, e);
--- functions/test-tangle/workflow.build.js
+++ functions/test-tangle/workflow.build.js
@@ -8,10 +8,11 @@ function getJobForFile(filePath) {
   const fileName = filePath
     .replaceAll('./test-tangle/', '')
     .replaceAll('/', '_')
-    .replaceAll('.', '-')
+    .replaceAll('.', '-');
   fs.appendFileSync(outputFile, `  ${fileName}:\n`);
+  fs.appendFileSync(outputFile, `    needs: npm-intstall\n`);
   fs.appendFileSync(outputFile, `    runs-on: ubuntu-latest\n`);
-  fs.appendFileSync(outputFile, `    timeout-minutes: 60\n`);
+  fs.appendFileSync(outputFile, `    timeout-minutes: 30\n`);
   fs.appendFileSync(outputFile, `    defaults:\n`);
   fs.appendFileSync(outputFile, `      run:\n`);
   fs.appendFileSync(outputFile, `        working-directory: functions\n`);
@@ -25,7 +26,13 @@ function getJobForFile(filePath) {
   fs.appendFileSync(outputFile, `      - uses: actions/setup-node@v1\n`);
   fs.appendFileSync(outputFile, `        with:\n`);
   fs.appendFileSync(outputFile, `          node-version: '16'\n`);
-  fs.appendFileSync(outputFile, `      - run: npm install\n`);
+  fs.appendFileSync(outputFile, `      - uses: actions/cache@v3\n`);
+  fs.appendFileSync(outputFile, `        with:\n`);
+  fs.appendFileSync(outputFile, `          path: '**/node_modules'\n`);
+  fs.appendFileSync(
+    outputFile,
+    `          key: \${{ runner.os }}-modules-\${{ hashFiles('**/package.json') }}\n`,
+  );
   fs.appendFileSync(outputFile, `      - run: npm install -g firebase-tools\n`);
   fs.appendFileSync(outputFile, `      - run: npm run build\n`);
   fs.appendFileSync(
@@ -47,8 +54,34 @@ try {
   fs.appendFileSync(outputFile, '  pull_request:\n');
   fs.appendFileSync(outputFile, '    paths:\n');
   fs.appendFileSync(outputFile, '      - functions/**\n\n');
-  
-  fs.appendFileSync(outputFile, 'jobs:\n\n');
+
+  fs.appendFileSync(outputFile, 'jobs:\n');
+  fs.appendFileSync(outputFile, '  npm-intstall:\n');
+  fs.appendFileSync(outputFile, '    runs-on: ubuntu-latest\n');
+  fs.appendFileSync(outputFile, '    timeout-minutes: 10\n');
+  fs.appendFileSync(outputFile, '    defaults:\n');
+  fs.appendFileSync(outputFile, '      run:\n');
+  fs.appendFileSync(outputFile, '        working-directory: functions\n');
+  fs.appendFileSync(outputFile, '    steps:\n');
+  fs.appendFileSync(outputFile, '      - uses: actions/checkout@v3\n');
+  fs.appendFileSync(outputFile, '      - uses: actions/setup-node@v3\n');
+  fs.appendFileSync(outputFile, '        with:\n');
+  fs.appendFileSync(outputFile, '          node-version: 16\n');
+  fs.appendFileSync(outputFile, '      - uses: actions/cache@v3\n');
+  fs.appendFileSync(outputFile, '        id: cache\n');
+  fs.appendFileSync(outputFile, '        with:\n');
+  fs.appendFileSync(outputFile, "          path: '**/node_modules'\n");
+  fs.appendFileSync(
+    outputFile,
+    "          key: ${{ runner.os }}-modules-${{ hashFiles('**/package.json') }}\n",
+  );
+  fs.appendFileSync(outputFile, '      - name: Install Dependencies\n');
+  fs.appendFileSync(
+    outputFile,
+    "        if: steps.cache.outputs.cache-hit != 'true'\n",
+  );
+  fs.appendFileSync(outputFile, '        run: npm install\n\n');
+
   glob.sync(`./test-tangle/**/*.spec.ts`).forEach(getJobForFile);
 } catch (e) {
   console.error(errorMsg, e);
