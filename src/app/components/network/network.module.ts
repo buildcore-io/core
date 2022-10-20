c7b17fbf074b9548e814b5096e23d324ba04a840
humaint <humaint@unchained.work>
2022-10-20 15:25:45 +1100

    button module added

--- src/app/components/network/network.module.ts
+++ src/app/components/network/network.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { IconModule } from '@components/icon/icon.module';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 import { SelectNetworkComponent } from './components/select-network/select-network.component';
 import { SelectedNetworkComponent } from './components/selected-network/selected-network.component';
 import { SendFundsComponent } from './components/send-funds/send-funds.component';
@@ -9,7 +10,8 @@ import { SendFundsComponent } from './components/send-funds/send-funds.component
   declarations: [SelectedNetworkComponent, SelectNetworkComponent, SendFundsComponent],
   imports: [
     CommonModule,
-    IconModule
+    IconModule,
+    NzButtonModule
   ],
   exports: [SelectedNetworkComponent, SelectNetworkComponent, SendFundsComponent],
 })
