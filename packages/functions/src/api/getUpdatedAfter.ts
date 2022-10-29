71329be9ec556da0c79d05b2d6794c33932c8802
adam_unchained <adam@unchained.work>
2022-10-29 15:06:21 -0700

    pretier

--- packages/functions/src/api/getUpdatedAfter.ts
+++ packages/functions/src/api/getUpdatedAfter.ts
@@ -1,7 +1,4 @@
-import {
-  GetUpdatedAfterRequest, PublicCollections,
-  PublicSubCollections
-} from '@soon/interfaces';
+import { GetUpdatedAfterRequest, PublicCollections, PublicSubCollections } from '@soon/interfaces';
 import dayjs from 'dayjs';
 import * as functions from 'firebase-functions';
 import Joi from 'joi';
@@ -28,43 +25,39 @@ export const getUpdatedAfter = async (req: functions.https.Request, res: functio
     return;
   }
 
-  const isSubCollectionQuery = (body.subCollection && body.uid);
-  const baseCollectionPath =
-      isSubCollectionQuery
-      ? `${body.collection}/${body.uid}/${body.subCollection}`
-      : body.collection;
+  const isSubCollectionQuery = body.subCollection && body.uid;
+  const baseCollectionPath = isSubCollectionQuery
+    ? `${body.collection}/${body.uid}/${body.subCollection}`
+    : body.collection;
 
   const updatedAfter = body.updatedAfter ? dayjs(body.updatedAfter) : dayjs().subtract(1, 'h');
   if (!isSubCollectionQuery && body.uid) {
     // They want to just monitor one record.
-    let query  = await admin
-      .firestore()
-      .collection(baseCollectionPath)
-      .where('uid', '==', body.uid);
+    let query = await admin.firestore().collection(baseCollectionPath).where('uid', '==', body.uid);
 
-      if (body.collection === PublicCollections.NFT) {
-        query = query.where('hidden', '==', false);
-      }
-  
-      if (body.collection === PublicCollections.TRANSACTION) {
-        query = query.where('isOrderType', '==', false);
-      }
+    if (body.collection === PublicCollections.NFT) {
+      query = query.where('hidden', '==', false);
+    }
 
-      query = query.limit(1);
-      const snap = await query.get();
-        const result = snap.docs
-        .map((d) => d.data())
-        .filter((d) => !isEmpty(d) && dayjs(d.data().updatedOn.toDate()).isAfter(updatedAfter))
-        .map((d) => ({ id: d.uid, ...d }));
-      res.send(result);
+    if (body.collection === PublicCollections.TRANSACTION) {
+      query = query.where('isOrderType', '==', false);
+    }
+
+    query = query.limit(1);
+    const snap = await query.get();
+    const result = snap.docs
+      .map((d) => d.data())
+      .filter((d) => !isEmpty(d) && dayjs(d.updatedOn.toDate()).isAfter(updatedAfter))
+      .map((d) => ({ id: d.uid, ...d }));
+    res.send(result);
   } else {
     const startAtSnap = await admin
-    .firestore()
-    .collection(baseCollectionPath)
-    .where('updatedOn', '>=', dateToTimestamp(updatedAfter.toDate()))
-    .orderBy('updatedOn')
-    .limit(1)
-    .get();
+      .firestore()
+      .collection(baseCollectionPath)
+      .where('updatedOn', '>=', dateToTimestamp(updatedAfter.toDate()))
+      .orderBy('updatedOn')
+      .limit(1)
+      .get();
 
     let query = admin
       .firestore()
