32d3777214b8936daf6394a2143ed8f29cc153d9
adam_unchained <adam@unchained.work>
2022-10-29 13:40:06 -0700

    added filtering on getUpdatedAfter

--- packages/functions/test/api/getUpdatedAfter.spec.ts
+++ packages/functions/test/api/getUpdatedAfter.spec.ts
@@ -7,10 +7,11 @@ import { dateToTimestamp } from '../../src/utils/dateTime.utils';
 import { getRandomEthAddress } from '../../src/utils/wallet.utils';
 
 describe('Get many by id', () => {
+  let uids;
   it('Should get all', async () => {
     const count = QUERY_MAX_LENGTH + 1;
     const updatedOn = dayjs().add(100, 'y');
-    const uids = Array.from(Array(count)).map(() => getRandomEthAddress());
+    uids = Array.from(Array(count)).map(() => getRandomEthAddress());
     const batch = admin.firestore().batch();
     uids.forEach((uid, i) => {
       const docRef = admin.firestore().doc(`${PublicCollections.MEMBER}/${uid}`);
@@ -56,6 +57,20 @@ describe('Get many by id', () => {
         const allHaveIds = body.reduce((acc: any, act: any) => acc && !isEmpty(act.id), true);
         expect(allHaveIds).toBe(true);
         expect(body.length).toBe(100);
+      },
+    } as any;
+    await getUpdatedAfter(req, res);
+  });
+
+  it('Should get one record, updatedAfter not provided', async () => {
+    const req = {
+      query: { collection: PublicCollections.MEMBER, fieldName: 'uid', fieldValue: uids[0] },
+    } as any;
+    const res = {
+      send: (body: any[]) => {
+        const allHaveIds = body.reduce((acc: any, act: any) => acc && !isEmpty(act.id), true);
+        expect(allHaveIds).toBe(true);
+        expect(body.length).toBe(1);
       },
     } as any;
     await getUpdatedAfter(req, res);
