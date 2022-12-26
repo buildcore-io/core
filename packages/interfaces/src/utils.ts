90700894788e0a2c985d6f8d17f1e0eeae2fdaef
Boldizsar Mezei <boldizsar@unchained.work>
2022-12-26 22:02:48 +0100

    Limit stake multiplier

--- packages/interfaces/src/utils.ts
+++ packages/interfaces/src/utils.ts
@@ -2,4 +2,7 @@ import { MAX_WEEKS_TO_STAKE } from './config';
 
 const M = 1 / (MAX_WEEKS_TO_STAKE - 1);
 const B = 2 - M * MAX_WEEKS_TO_STAKE;
-export const calcStakedMultiplier = (weeks: number) => Number((M * (weeks || 1) + B).toFixed(8));
+export const calcStakedMultiplier = (weeks: number) => {
+  const multiplier = Number((M * (weeks || 1) + B).toFixed(8));
+  return Math.min(multiplier, 2);
+};
