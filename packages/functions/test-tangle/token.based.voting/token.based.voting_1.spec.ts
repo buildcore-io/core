3b9e3063905215e47f2c30b84bd9e50a97eb4483
Boldizsar Mezei <boldizsar@unchained.work>
2023-01-12 19:16:17 +0100

    Add tests

--- packages/functions/test-tangle/token.based.voting/token.based.voting_1.spec.ts
+++ packages/functions/test-tangle/token.based.voting/token.based.voting_1.spec.ts
@@ -1,7 +1,9 @@
-import { MIN_IOTA_AMOUNT } from '@soonaverse/interfaces';
+import { COL, Member, MIN_IOTA_AMOUNT, WenError } from '@soonaverse/interfaces';
 import dayjs from 'dayjs';
+import admin from '../../src/admin.config';
 import { voteOnProposal } from '../../src/controls/proposal/vote.on.proposal';
-import { mockWalletReturnValue, wait } from '../../test/controls/common';
+import { getAddress } from '../../src/utils/address.utils';
+import { expectThrow, mockWalletReturnValue, wait } from '../../test/controls/common';
 import { testEnv } from '../../test/set-up';
 import { requestFundsFromFaucet, requestMintedTokenFromFaucet } from '../faucet';
 import { Helper, MINTED_TOKEN_ID, VAULT_MNEMONIC } from '../token.based.voting/Helper';
@@ -190,5 +192,71 @@ describe('Token based voting', () => {
     });
     await helper.assertProposalWeights(5, 5);
     await helper.assertProposalMemberWeights(helper.guardian, 0, 2);
+  });
+
+  it('Should throw, can not vote after end date', async () => {
+    await helper.updatePropoasalDates(dayjs().subtract(2, 'd'), dayjs().subtract(1, 'd'));
+    mockWalletReturnValue(helper.walletSpy, helper.guardian, {
+      uid: helper.proposal!.uid,
+      values: [1],
+    });
+    await expectThrow(testEnv.wrap(voteOnProposal)({}), WenError.vote_is_no_longer_active.key);
+  });
+
+  it('Should throw, can only vote 24 hours before', async () => {
+    await helper.updatePropoasalDates(dayjs().add(3, 'd'), dayjs().add(5, 'd'));
+    mockWalletReturnValue(helper.walletSpy, helper.guardian, {
+      uid: helper.proposal!.uid,
+      values: [1],
+    });
+    await expectThrow(testEnv.wrap(voteOnProposal)({}), WenError.vote_is_no_longer_active.key);
+  });
+
+  it('Should vote, spend, other person votes with it', async () => {
+    mockWalletReturnValue(helper.walletSpy, helper.guardian, {
+      uid: helper.proposal!.uid,
+      values: [1],
+    });
+    let voteTransactionOrder = await testEnv.wrap(voteOnProposal)({});
+    await helper.sendTokensToVote(voteTransactionOrder.payload.targetAddress);
+    let credit = await helper.awaitVoteTransactionCreditIsConfirmed(
+      voteTransactionOrder.payload.targetAddress,
+    );
+
+    let voteTransaction = await helper.getVoteTransactionForCredit(credit.uid);
+    expect(voteTransaction.payload.weight).toBe(10);
+    await helper.assertProposalWeights(10, 10);
+    await helper.assertProposalMemberWeights(helper.guardian, 10, 1);
+
+    await helper.updatePropoasalDates(dayjs().subtract(2, 'd'), dayjs().add(2, 'd'));
+    await helper.updateVoteTranCreatedOn(voteTransaction.uid, dayjs().subtract(3, 'd'));
+
+    const memberDocRef = admin.firestore().doc(`${COL.MEMBER}/${helper.member}`);
+    const member = <Member>(await memberDocRef.get()).data();
+    const memberAddress = await helper.walletService!.getAddressDetails(
+      getAddress(member, helper.network),
+    );
+    await helper.sendTokensToVote(memberAddress.bech32);
+    await wait(async () => {
+      const voteTransaction = await helper.getVoteTransactionForCredit(credit.uid);
+      return voteTransaction.payload.weight === 5;
+    });
+    await helper.assertProposalWeights(5, 5);
+    await helper.assertProposalMemberWeights(helper.guardian, 5, 1);
+
+    mockWalletReturnValue(helper.walletSpy, helper.member, {
+      uid: helper.proposal!.uid,
+      values: [1],
+    });
+    voteTransactionOrder = await testEnv.wrap(voteOnProposal)({});
+    await helper.sendTokensToVote(voteTransactionOrder.payload.targetAddress, 10, memberAddress);
+    credit = await helper.awaitVoteTransactionCreditIsConfirmed(
+      voteTransactionOrder.payload.targetAddress,
+    );
+
+    voteTransaction = await helper.getVoteTransactionForCredit(credit.uid);
+    expect(voteTransaction.payload.weight).toBe(5);
+    await helper.assertProposalWeights(10, 10);
+    await helper.assertProposalMemberWeights(helper.member, 5, 1);
   });
 });
