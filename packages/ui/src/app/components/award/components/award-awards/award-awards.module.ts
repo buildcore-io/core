ac909d785f29050a75cdc03b97764259a771e6f0
humaint <humaint@unchained.work>
2023-02-08 13:48:17 +1100

    award CSV button

--- packages/ui/src/app/components/award/components/award-awards/award-awards.module.ts
+++ packages/ui/src/app/components/award/components/award-awards/award-awards.module.ts
@@ -4,11 +4,13 @@ import { IpfsBadgeModule } from '@core/pipes/ipfs-badge/ipfs-badge.module';
 import { NzAvatarModule } from 'ng-zorro-antd/avatar';
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzTagModule } from 'ng-zorro-antd/tag';
+import { IconModule } from '@components/icon/icon.module';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 import { AwardAwardsComponent } from './award-awards.component';
 
 @NgModule({
   declarations: [AwardAwardsComponent],
-  imports: [CommonModule, NzCardModule, NzAvatarModule, IpfsBadgeModule, NzTagModule],
+  imports: [CommonModule, NzCardModule, NzAvatarModule, IpfsBadgeModule, NzTagModule, IconModule, NzButtonModule],
   exports: [AwardAwardsComponent],
 })
 export class AwardAwardsModule {}
