fa27bb08954672e9ad7192fdfced63bbfa9b8936
adam_unchained <adam@unchained.work>
2023-02-15 21:38:29 -0800

    fixed reputation

--- packages/ui/src/app/pages/member/pages/badges/badges.page.ts
+++ packages/ui/src/app/pages/member/pages/badges/badges.page.ts
@@ -1,12 +1,12 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { AuthService } from '@components/auth/services/auth.service';
 import { CacheService } from '@core/services/cache/cache.service';
 import { DeviceService } from '@core/services/device';
 import { PreviewImageService } from '@core/services/preview-image';
 import { UnitsService } from '@core/services/units';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
-import { Transaction } from '@soonaverse/interfaces';
-import { BehaviorSubject, combineLatest, map, Observable } from 'rxjs';
+import { GLOBAL_DEBOUNCE_TIME, Transaction } from '@soonaverse/interfaces';
+import { BehaviorSubject, combineLatest, debounceTime, map } from 'rxjs';
 import { DataService } from './../../services/data.service';
 
 interface TokensBreakdown {
@@ -28,7 +28,13 @@ interface DetailedList {
   styleUrls: ['./badges.page.less'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BadgesPage {
+export class BadgesPage implements OnInit {
+  public detailedReputationList$: BehaviorSubject<DetailedList[]> = new BehaviorSubject<
+    DetailedList[]
+  >([]);
+  public totalReputationList$: BehaviorSubject<TokensBreakdown[]> = new BehaviorSubject<
+    TokensBreakdown[]
+  >([]);
   constructor(
     private auth: AuthService,
     public cache: CacheService,
@@ -40,82 +46,87 @@ export class BadgesPage {
     // none.
   }
 
-  public get isLoggedIn$(): BehaviorSubject<boolean> {
-    return this.auth.isLoggedIn$;
-  }
+  public ngOnInit(): void {
+    combineLatest([this.data.spaces$, this.data.member$])
+      .pipe(
+        map(([spaces, member]) => {
+          const output: DetailedList[] = [];
+          for (const s in member?.spaces || {}) {
+            if (Object.prototype.hasOwnProperty.call(member!.spaces, s)) {
+              const rec = member!.spaces![s];
+              const space = spaces?.find((sd) => {
+                return sd.uid === s;
+              });
 
-  public trackByUid(index: number, item: Transaction) {
-    return item.uid;
-  }
+              const out: DetailedList = {
+                spaceUid: s,
+                spaceAvatarUrl: space?.avatarUrl,
+                spaceName: space?.name,
+                rewards: [],
+              };
 
-  public get detailedReputationList$(): Observable<DetailedList[]> {
-    return combineLatest([this.data.spaces$, this.data.member$]).pipe(
-      untilDestroyed(this),
-      map(([spaces, member]) => {
-        const output: DetailedList[] = [];
-        for (const s in member?.spaces || {}) {
-          if (Object.prototype.hasOwnProperty.call(member!.spaces, s)) {
-            const rec = member!.spaces![s];
-            const space = spaces?.find((sd) => {
-              return sd.uid === s;
-            });
-
-            const out: DetailedList = {
-              spaceUid: s,
-              spaceAvatarUrl: space?.avatarUrl,
-              spaceName: space?.name,
-              rewards: [],
-            };
-
-            for (const t in rec.awardStat) {
-              if (Object.prototype.hasOwnProperty.call(rec.awardStat, t)) {
-                out.rewards.push({
-                  totalTokenRewards: rec.awardStat[t].totalReward || 0,
-                  completedAwards: rec.awardStat[t].completed || 0,
-                  uid: t,
-                });
-              }
-            }
-
-            output.push(out);
-          }
-        }
-
-        return output;
-      }),
-    );
-  }
-
-  public get getTotalReputationList$(): Observable<TokensBreakdown[]> {
-    return combineLatest([this.data.spaces$, this.data.member$]).pipe(
-      untilDestroyed(this),
-      map(([spaces, member]) => {
-        const output: TokensBreakdown[] = [];
-        for (const s in member?.spaces || {}) {
-          if (Object.prototype.hasOwnProperty.call(member!.spaces, s)) {
-            const rec = member!.spaces![s];
-            for (const t in rec.awardStat) {
-              if (Object.prototype.hasOwnProperty.call(rec.awardStat, t)) {
-                const recExists = output.find((tes) => {
-                  return tes.uid === t;
-                });
-                if (recExists) {
-                  recExists.totalTokenRewards += rec.awardStat[t].totalReward || 0;
-                  recExists.completedAwards += rec.awardStat[t].completed || 0;
-                } else {
-                  output.push({
+              for (const t in rec.awardStat) {
+                if (Object.prototype.hasOwnProperty.call(rec.awardStat, t)) {
+                  out.rewards.push({
                     totalTokenRewards: rec.awardStat[t].totalReward || 0,
                     completedAwards: rec.awardStat[t].completed || 0,
                     uid: t,
                   });
                 }
               }
+
+              output.push(out);
             }
           }
-        }
 
-        return output;
-      }),
-    );
+          return output;
+        }),
+        untilDestroyed(this),
+        debounceTime(GLOBAL_DEBOUNCE_TIME),
+      )
+      .subscribe(this.detailedReputationList$);
+
+    combineLatest([this.data.spaces$, this.data.member$])
+      .pipe(
+        map(([spaces, member]) => {
+          console.log('aaa');
+          const output: TokensBreakdown[] = [];
+          for (const s in member?.spaces || {}) {
+            if (Object.prototype.hasOwnProperty.call(member!.spaces, s)) {
+              const rec = member!.spaces![s];
+              for (const t in rec.awardStat) {
+                if (Object.prototype.hasOwnProperty.call(rec.awardStat, t)) {
+                  const recExists = output.find((tes) => {
+                    return tes.uid === t;
+                  });
+                  if (recExists) {
+                    recExists.totalTokenRewards += rec.awardStat[t].totalReward || 0;
+                    recExists.completedAwards += rec.awardStat[t].completed || 0;
+                  } else {
+                    output.push({
+                      totalTokenRewards: rec.awardStat[t].totalReward || 0,
+                      completedAwards: rec.awardStat[t].completed || 0,
+                      uid: t,
+                    });
+                  }
+                }
+              }
+            }
+          }
+
+          return output;
+        }),
+        untilDestroyed(this),
+        debounceTime(GLOBAL_DEBOUNCE_TIME),
+      )
+      .subscribe(this.totalReputationList$);
+  }
+
+  public get isLoggedIn$(): BehaviorSubject<boolean> {
+    return this.auth.isLoggedIn$;
+  }
+
+  public trackByUid(index: number, item: Transaction) {
+    return item.uid;
   }
 }
