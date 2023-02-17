7126c29062548c633209b80ba845c8ecf6e6003d
humaint <humaint@unchained.work>
2023-02-17 17:15:21 +1100

    cleaning

--- packages/ui/src/app/components/award/components/award-awards/award-awards.module.ts
+++ packages/ui/src/app/components/award/components/award-awards/award-awards.module.ts
@@ -4,6 +4,7 @@ import { IconModule } from '@components/icon/icon.module';
 import { IpfsBadgeModule } from '@core/pipes/ipfs-badge/ipfs-badge.module';
 import { NzAvatarModule } from 'ng-zorro-antd/avatar';
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzTagModule } from 'ng-zorro-antd/tag';
 import { AwardGiveBadgesModule } from '../award-give-badges/award-give-badges.module';
@@ -17,6 +18,7 @@ import { AwardAwardsComponent } from './award-awards.component';
     NzAvatarModule,
     IpfsBadgeModule,
     NzTagModule,
+    NzToolTipModule,
     IconModule,
     NzButtonModule,
     AwardGiveBadgesModule,
