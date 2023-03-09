aefc51e46a9ecc082e7cb413fcd8a0a71cd4f647
Boldizsar Mezei <boldizsar@unchained.work>
2023-03-09 15:36:53 +0100

    Fix media cron

--- packages/functions/src/cron/media.cron.ts
+++ packages/functions/src/cron/media.cron.ts
@@ -1,4 +1,13 @@
-import { Award, COL, Collection, MediaStatus, Nft, Space, Token } from '@soonaverse/interfaces';
+import {
+  Award,
+  COL,
+  Collection,
+  MAX_WALLET_RETRY,
+  MediaStatus,
+  Nft,
+  Space,
+  Token,
+} from '@soonaverse/interfaces';
 import * as functions from 'firebase-functions';
 import admin, { inc } from '../admin.config';
 import { awardToIpfsMetadata } from '../services/payment/award/award-service';
@@ -15,28 +24,27 @@ import { spaceToIpfsMetadata } from '../utils/space.utils';
 export const MEDIA_UPLOAD_BACH_SIZE = 30;
 export const uploadMediaToWeb3 = async () => {
   let batchSize = MEDIA_UPLOAD_BACH_SIZE;
-  const nftUpload = await uploadMedia(COL.NFT, batchSize, uploadNftMedia);
-  batchSize -= nftUpload.size;
 
-  const tokenUpload = await uploadMedia(COL.TOKEN, batchSize, uploadTokenMedia);
-  batchSize -= tokenUpload.size;
+  const props = [
+    { col: COL.NFT, func: uploadNftMedia },
+    { col: COL.TOKEN, func: uploadTokenMedia },
+    { col: COL.COLLECTION, func: uploadCollectionMedia },
+    { col: COL.AWARD, func: uploadAwardMedia },
+    { col: COL.SPACE, func: uploadSpaceMedia },
+  ];
 
-  const collectionUpload = await uploadMedia(COL.COLLECTION, batchSize, uploadCollectionMedia);
-  batchSize -= collectionUpload.size;
+  const promises: Promise<void>[] = [];
+  for (const prop of props) {
+    const upload = await uploadMedia(
+      prop.col,
+      batchSize,
+      prop.func as (data: unknown) => Promise<void>,
+    );
+    batchSize -= upload.size;
+    promises.push(...upload.promises);
+  }
 
-  const awardUpload = await uploadMedia(COL.AWARD, batchSize, uploadAwardMedia);
-  batchSize -= awardUpload.size;
-
-  const spaceUpload = await uploadMedia(COL.SPACE, batchSize, uploadSpaceMedia);
-  batchSize -= spaceUpload.size;
-
-  await Promise.all([
-    ...nftUpload.promises,
-    ...tokenUpload.promises,
-    ...collectionUpload.promises,
-    ...awardUpload.promises,
-    ...spaceUpload.promises,
-  ]);
+  await Promise.allSettled(promises);
 
   return batchSize;
 };
@@ -50,58 +58,49 @@ const uploadMedia = async <T>(
     return { size: 0, promises: [] as Promise<void>[] };
   }
   const snap = await pendingUploadQuery(col, batchSize).get();
-  const promises = snap.docs.map((d) => uploadFunc(<T>d.data()));
+  const promises = snap.docs.map(async (d) => {
+    try {
+      return await uploadFunc(<T>d.data());
+    } catch (error) {
+      await setMediaStatusToError(col, d.id, d.data()?.mediaUploadErrorCount || 0, error);
+    }
+  });
   return { size: snap.size, promises };
 };
 
 const uploadNftMedia = async (nft: Nft) => {
   const nftDocRef = admin.firestore().doc(`${COL.NFT}/${nft.uid}`);
-  try {
-    const collectionDocRef = admin.firestore().doc(`${COL.COLLECTION}/${nft.collection}`);
-    const collection = <Collection>(await collectionDocRef.get()).data();
-    const metadata = nftToIpfsMetadata(collection, nft);
-    const { car, ...ipfs } = await downloadMediaAndPackCar(nft.uid, nft.media, metadata);
-    await putCar(car);
+  const collectionDocRef = admin.firestore().doc(`${COL.COLLECTION}/${nft.collection}`);
+  const collection = <Collection>(await collectionDocRef.get()).data();
+  const metadata = nftToIpfsMetadata(collection, nft);
+  const { car, ...ipfs } = await downloadMediaAndPackCar(nft.uid, nft.media, metadata);
+  await putCar(car);
 
-    const batch = admin.firestore().batch();
-    batch.update(collectionDocRef, uOn({ 'mintingData.nftMediaToUpload': inc(-1) }));
-    batch.update(nftDocRef, uOn({ mediaStatus: MediaStatus.UPLOADED, ...ipfs }));
-    await batch.commit();
-  } catch (error) {
-    functions.logger.error(nft.uid, 'Nft media upload error', error);
-    await nftDocRef.update(uOn({ mediaStatus: MediaStatus.ERROR }));
-  }
+  const batch = admin.firestore().batch();
+  batch.update(collectionDocRef, uOn({ 'mintingData.nftMediaToUpload': inc(-1) }));
+  batch.update(nftDocRef, uOn({ mediaStatus: MediaStatus.UPLOADED, ...ipfs }));
+  await batch.commit();
 };
 
 const uploadTokenMedia = async (token: Token) => {
   const tokenDocRef = admin.firestore().doc(`${COL.TOKEN}/${token.uid}`);
-  try {
-    const metadata = tokenToIpfsMetadata(token);
-    const { car, ...ipfs } = await downloadMediaAndPackCar(token.uid, token.icon!, metadata);
-    await putCar(car);
-    await tokenDocRef.update(uOn({ mediaStatus: MediaStatus.UPLOADED, ...ipfs }));
-  } catch (error) {
-    functions.logger.error(token.uid, 'Token media upload error', error);
-    await tokenDocRef.update(uOn({ mediaStatus: MediaStatus.ERROR }));
-  }
+  const metadata = tokenToIpfsMetadata(token);
+  const { car, ...ipfs } = await downloadMediaAndPackCar(token.uid, token.icon!, metadata);
+  await putCar(car);
+  await tokenDocRef.update(uOn({ mediaStatus: MediaStatus.UPLOADED, ...ipfs }));
 };
 
 const uploadCollectionMedia = async (collection: Collection) => {
   const collectionDocRef = admin.firestore().doc(`${COL.COLLECTION}/${collection.uid}`);
-  try {
-    const metadata = collectionToIpfsMetadata(collection);
-    const { car, ...ipfs } = await downloadMediaAndPackCar(
-      collection.uid,
-      collection.bannerUrl,
-      metadata,
-    );
-    await putCar(car);
+  const metadata = collectionToIpfsMetadata(collection);
+  const { car, ...ipfs } = await downloadMediaAndPackCar(
+    collection.uid,
+    collection.bannerUrl,
+    metadata,
+  );
+  await putCar(car);
 
-    await collectionDocRef.update(uOn({ mediaStatus: MediaStatus.UPLOADED, ...ipfs }));
-  } catch (error) {
-    functions.logger.error(collection.uid, 'Collection bannerUrl upload error', error);
-    await collectionDocRef.update(uOn({ mediaStatus: MediaStatus.ERROR }));
-  }
+  await collectionDocRef.update(uOn({ mediaStatus: MediaStatus.UPLOADED, ...ipfs }));
 };
 
 const uploadAwardMedia = async (award: Award) => {
@@ -109,30 +108,19 @@ const uploadAwardMedia = async (award: Award) => {
     return;
   }
   const awardDocRef = admin.firestore().doc(`${COL.AWARD}/${award.uid}`);
-  try {
-    const metadata = awardToIpfsMetadata(award);
-    const { car, ...ipfs } = await downloadMediaAndPackCar(award.uid, award.badge.image, metadata);
-    await putCar(car);
+  const metadata = awardToIpfsMetadata(award);
+  const { car, ...ipfs } = await downloadMediaAndPackCar(award.uid, award.badge.image, metadata);
+  await putCar(car);
 
-    await awardDocRef.update(uOn({ mediaStatus: MediaStatus.UPLOADED, ...ipfs }));
-  } catch (error) {
-    functions.logger.error(award.uid, 'Award badge image upload error', error);
-    await awardDocRef.update(uOn({ mediaStatus: MediaStatus.ERROR }));
-  }
+  await awardDocRef.update(uOn({ mediaStatus: MediaStatus.UPLOADED, ...ipfs }));
 };
 
 const uploadSpaceMedia = async (space: Space) => {
   const spaceDocRef = admin.firestore().doc(`${COL.SPACE}/${space.uid}`);
-  try {
-    const metadata = spaceToIpfsMetadata(space);
-    const { car, ...ipfs } = await downloadMediaAndPackCar(space.uid, space.bannerUrl!, metadata);
-    await putCar(car);
-
-    await spaceDocRef.update(uOn({ mediaStatus: MediaStatus.UPLOADED, ...ipfs }));
-  } catch (error) {
-    functions.logger.error(space.uid, 'Space image upload error', error);
-    await spaceDocRef.update(uOn({ mediaStatus: MediaStatus.ERROR }));
-  }
+  const metadata = spaceToIpfsMetadata(space);
+  const { car, ...ipfs } = await downloadMediaAndPackCar(space.uid, space.bannerUrl!, metadata);
+  await putCar(car);
+  await spaceDocRef.update(uOn({ mediaStatus: MediaStatus.UPLOADED, ...ipfs }));
 };
 
 const pendingUploadQuery = (col: COL, batchSize: number) =>
@@ -141,3 +129,13 @@ const pendingUploadQuery = (col: COL, batchSize: number) =>
     .collection(col)
     .where('mediaStatus', '==', MediaStatus.PENDING_UPLOAD)
     .limit(batchSize);
+
+const setMediaStatusToError = async (col: COL, uid: string, errorCount: number, error: unknown) => {
+  const data = { mediaUploadErrorCount: inc(1), mediaStatus: MediaStatus.PENDING_UPLOAD };
+  if (errorCount >= MAX_WALLET_RETRY) {
+    data.mediaStatus = MediaStatus.ERROR;
+    functions.logger.error(col, uid, 'Image upload error', error);
+  }
+  const docRef = admin.firestore().doc(`${col}/${uid}`);
+  await docRef.update(uOn(data));
+};
