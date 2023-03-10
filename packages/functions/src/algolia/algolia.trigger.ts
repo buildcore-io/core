4b514c2f0993127330afa65e5c6af7c23f818f5a
adam_unchained <adam@unchained.work>
2023-03-09 19:51:53 -0800

    scalling tweak

--- packages/functions/src/algolia/algolia.trigger.ts
+++ packages/functions/src/algolia/algolia.trigger.ts
@@ -1,20 +1,17 @@
-import { ALGOLIA_COLLECTIONS, WEN_FUNC } from '@soonaverse/interfaces';
+import { ALGOLIA_COLLECTIONS } from '@soonaverse/interfaces';
 import algoliasearch from 'algoliasearch';
 import * as functions from 'firebase-functions';
-import { RuntimeOptions } from 'firebase-functions';
-import { scale } from '../scale.settings';
+import { scaleAlgolia } from '../scale.settings';
 import { algoliaAppId, algoliaKey, isEmulatorEnv } from '../utils/config.utils';
 import { docToAlgoliaData } from './firestore.to.algolia';
 
 const client = algoliasearch(algoliaAppId(), algoliaKey());
 
-const RUN_WITH: RuntimeOptions = {
-  minInstances: scale(WEN_FUNC.algolia),
-};
-
 export const algoliaTrigger = ALGOLIA_COLLECTIONS.map((col) => ({
   [col]: functions
-    .runWith(RUN_WITH)
+    .runWith({
+      minInstances: scaleAlgolia(col),
+    })
     .firestore.document(col + '/{documentId}')
     .onWrite(async (change) => {
       if (isEmulatorEnv()) {
