8dbcc39d5cfd4b001218a7c6339a77fc1d02d618
adam_unchained <adam@unchained.work>
2023-03-10 17:22:24 -0800

    fixed test

--- packages/lib/test/invalid.route.spec.ts
+++ packages/lib/test/invalid.route.spec.ts
@@ -1,10 +1,6 @@
 describe('Invalid route test', () => {
   it('Should return 404', async () => {
-    try {
-      await fetch('https://soonaverse-dev.web.app/api/asd', {});
-      expect('').toBe('Should be blocked');
-    } catch (error: any) {
-      expect(error).toBe('Not Found');
-    }
+    const of = await fetch('https://soonaverse-dev.web.app/api/asd', {});
+    expect(of.ok).toBe(false);
   });
 });
