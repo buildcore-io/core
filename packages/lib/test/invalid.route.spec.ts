de0df55588160197b6a8ebb665a34708e515500b
adam_unchained <adam@unchained.work>
2023-03-10 16:45:33 -0800

    fixed spec

--- packages/lib/test/invalid.route.spec.ts
+++ packages/lib/test/invalid.route.spec.ts
@@ -1,9 +1,9 @@
-import { fetch } from '../src/fetch.utils';
+import { wrappedFetch } from '../src/fetch.utils';
 
 describe('Invalid route test', () => {
   it('Should return 404', async () => {
     try {
-      await fetch('https://soonaverse-dev.web.app/api/asd', {});
+      await wrappedFetch('https://soonaverse-dev.web.app/api/asd', {});
       fail();
     } catch (error: any) {
       expect(error).toBe('Not Found');
