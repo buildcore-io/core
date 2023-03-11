a69d566a5171ce1dc84728397ae348fc48a7eda6
adam_unchained <adam@unchained.work>
2023-03-10 16:51:15 -0800

    switched to native fetch

--- packages/lib/test/invalid.route.spec.ts
+++ packages/lib/test/invalid.route.spec.ts
@@ -1,9 +1,7 @@
-import { wrappedFetch } from '../src/fetch.utils';
-
 describe('Invalid route test', () => {
   it('Should return 404', async () => {
     try {
-      await wrappedFetch('https://soonaverse-dev.web.app/api/asd', {});
+      await fetch('https://soonaverse-dev.web.app/api/asd', {});
       fail();
     } catch (error: any) {
       expect(error).toBe('Not Found');
