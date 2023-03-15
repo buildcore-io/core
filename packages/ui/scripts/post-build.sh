f2b5e9f29aa47ea73fe91812dba65bc75e21966a
Adam <adam@kundrat.eu>
2023-03-15 10:04:11 -0700

    Revert "init commit"

--- packages/ui/scripts/post-build.sh
+++ packages/ui/scripts/post-build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-# Localization disabled.
-# mv ./dist/soonaverse ./dist/localized
-# cp -r ./dist/localized/en/* ./dist/
-# mv ./dist/localized/zh-CN ./dist/localized/zh_CN
+mv ./dist/soonaverse ./dist/localized
+cp -r ./dist/localized/en/* ./dist/
+# mv ./dist/localized/pt-BR ./dist/localized/pt_BR
+# mv ./dist/localized/pt-PT ./dist/localized/pt_PT
+mv ./dist/localized/zh-CN ./dist/localized/zh_CN
+# mv ./dist/localized/zh-TW ./dist/localized/zh_TW
--- packages/ui/src/app/@core/utils/language.util.ts
+++ packages/ui/src/app/@core/utils/language.util.ts
@@ -1,13 +1,25 @@
 /* eslint-disable camelcase */
-import { en_GB } from 'ng-zorro-antd/i18n';
-
-// es_ES,
-// fr_FR,
-// it_IT,
-// ko_KR,
-// nl_NL,
-// zh_CN
-// de_DE,
+import {
+  de_DE,
+  en_GB,
+  es_ES,
+  fr_FR,
+  it_IT,
+  ko_KR,
+  // hu_HU,
+  // ja_JP,
+  // pl_PL,
+  // pt_BR,
+  // pt_PT,
+  // ru_RU,
+  // tr_TR,
+  // uk_UA,
+  // zh_TW,
+  // ar_EG,
+  // cs_CZ,
+  nl_NL,
+  zh_CN,
+} from 'ng-zorro-antd/i18n';
 
 export interface LanguagesType {
   [key: string]: {
@@ -23,39 +35,100 @@ export const Languages: LanguagesType = {
     title: 'EN',
     firebase: 'en',
   },
-  // de: {
-  //   ngZorro: de_DE,
-  //   title: 'DE',
-  //   firebase: 'de',
+  de: {
+    ngZorro: de_DE,
+    title: 'DE',
+    firebase: 'de',
+  },
+  es: {
+    ngZorro: es_ES,
+    title: 'ES',
+    firebase: 'es',
+  },
+  fr: {
+    ngZorro: fr_FR,
+    title: 'FR',
+    firebase: 'fr',
+  },
+  it: {
+    ngZorro: it_IT,
+    title: 'IT',
+    firebase: 'it',
+  },
+  ko: {
+    ngZorro: ko_KR,
+    title: 'KO',
+    firebase: 'ko',
+  },
+  nl: {
+    ngZorro: nl_NL,
+    title: 'NL',
+    firebase: 'nl',
+  },
+  zh_cn: {
+    ngZorro: zh_CN,
+    title: '简中',
+    firebase: 'cn',
+  },
+  // ar_eg: {
+  //   ngZorro: ar_EG,
+  //   title: 'AR-EG',
+  //   firebase: 'eg',
   // },
-  // es: {
-  //   ngZorro: es_ES,
-  //   title: 'ES',
-  //   firebase: 'es',
+  // ar_sa: {
+  //   // There is no ar_SA in ng-zorro-antd
+  //   ngZorro: ar_EG,
+  //   title: 'AR-SA',
+  //   firebase: 'sa',
   // },
-  // fr: {
-  //   ngZorro: fr_FR,
-  //   title: 'FR',
-  //   firebase: 'fr',
+  // cs: {
+  //   ngZorro: cs_CZ,
+  //   title: 'CS',
+  //   firebase: 'cs',
   // },
-  // it: {
-  //   ngZorro: it_IT,
-  //   title: 'IT',
-  //   firebase: 'it',
+  // hu: {
+  //   ngZorro: hu_HU,
+  //   title: 'HU',
+  //   firebase: 'hu',
   // },
-  // ko: {
-  //   ngZorro: ko_KR,
-  //   title: 'KO',
-  //   firebase: 'ko',
+  // ja: {
+  //   ngZorro: ja_JP,
+  //   title: 'JA',
+  //   firebase: 'ja',
   // },
-  // nl: {
-  //   ngZorro: nl_NL,
-  //   title: 'NL',
-  //   firebase: 'nl',
+  // pl: {
+  //   ngZorro: pl_PL,
+  //   title: 'PL',
+  //   firebase: 'pl',
   // },
-  // zh_cn: {
-  //   ngZorro: zh_CN,
-  //   title: '简中',
-  //   firebase: 'cn',
-  // }
+  // pt_br: {
+  //   ngZorro: pt_BR,
+  //   title: 'PT-BR',
+  //   firebase: 'br',
+  // },
+  // pt_pt: {
+  //   ngZorro: pt_PT,
+  //   title: 'PT-PT',
+  //   firebase: 'pt',
+  // },
+  // ru: {
+  //   ngZorro: ru_RU,
+  //   title: 'RU',
+  //   firebase: 'ru',
+  // },
+  // tr: {
+  //   ngZorro: tr_TR,
+  //   title: 'TR',
+  //   firebase: 'tr',
+  // },
+  // uk: {
+  //   ngZorro: uk_UA,
+  //   title: 'UK',
+  //   firebase: 'uk',
+  // },
+  // zh_tw: {
+  //   ngZorro: zh_TW,
+  //   title: '繁中',
+  //   firebase: 'tw',
+  // },
 };
