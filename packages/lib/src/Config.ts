81eb80a0770764c8db0cae5f04de0fab8dbc671e
Boldizsar Mezei <boldizsar@unchained.work>
2023-06-22 18:18:24 +0200

    Set build5 url

--- packages/lib/src/Config.ts
+++ packages/lib/src/Config.ts
@@ -3,8 +3,8 @@ import { wrappedFetch } from './fetch.utils';
 import { randomString } from './utils';
 
 export enum Build5Env {
-  PROD = 'https://api.soonaverse.com/api',
-  TEST = 'https://api-wen2.soonaverse.com/api',
+  PROD = 'https://api.build5.com/api',
+  TEST = 'https://api-wen.build5.com/api',
   DEV = 'https://soonaverse-dev.web.app/api',
   LOCAL = 'http://127.0.0.1:5001/soonaverse-dev/us-central1/api',
 }
--- packages/lib/test/invalid.route.spec.ts
+++ packages/lib/test/invalid.route.spec.ts
@@ -1,6 +1,6 @@
 describe('Invalid route test', () => {
   it('Should return 404', async () => {
-    const of = await fetch('https://api-wen2.soonaverse.com/api/asd', {});
+    const of = await fetch('https://api-wen.build5.com/api/asd', {});
     expect(of.ok).toBe(false);
   });
 });
