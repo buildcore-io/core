ef68e17da30e5ef110a1238340e9c43e02dd2c67
Boldizsar Mezei <boldizsar@unchained.work>
2023-06-27 23:56:05 +0200

    Docusaurus init
    added building comomand
    
    Fixes
    
    test
    
    Fixes
    
    Cleanup
    
    Fix
    
    Fixes

---
+++ docs/docusaurus.config.js
@@ -0,0 +1,88 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Build5 docs',
+  tagline: 'Build5 documentation',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-test-site.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'build5', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  plugins: [
+    [
+      'docusaurus-plugin-typedoc',
+
+      // Plugin / TypeDoc options
+      {
+        entryPoints: ['../packages/interfaces/src/index.ts'],
+        tsconfig: '../packages/interfaces/tsconfig.json',
+      },
+    ],
+  ],
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: { routeBasePath: '/', sidebarPath: require.resolve('./sidebars.js') },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'Build5',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Docs',
+          },
+        ],
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
--- packages/interfaces/src/api/post/address.ts
+++ packages/interfaces/src/api/post/address.ts
@@ -1,6 +1,17 @@
 import { Network } from '../../models';
 
+/**
+ * Http request to create an address validation order
+ */
 export interface AddressValidationRequest {
+  /**
+   * Creates an address validation order for the given space.
+   * If undefined the address validation order will be created for the member
+   */
   space?: string;
+  /**
+   * Specified the network for validatin an address.
+   * If undefined will default to IOTA
+   */
   network?: Network;
 }
