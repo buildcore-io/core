dc503cee7b3ab2c30adf093dfdec1364645bcf0e
Boldizsar Mezei <boldizsar@unchained.work>
2023-09-06 12:27:32 +0200

    Allow extended auction
    Fixes
    
    Fixes

--- packages/functions/src/runtime/firebase/nft/NftSetForSaleRequestSchema.ts
+++ packages/functions/src/runtime/firebase/nft/NftSetForSaleRequestSchema.ts
@@ -39,6 +39,14 @@ export const setNftForSaleSchema = toJoiObject<NftSetForSaleRequest>({
     .description(
       `Millisecond value of the auction length. Minimum ${TRANSACTION_AUTO_EXPIRY_MS}, maximum ${TRANSACTION_MAX_EXPIRY_MS}`,
     ),
+  extendedAuctionLength: Joi.number()
+    .min(TRANSACTION_AUTO_EXPIRY_MS)
+    .max(TRANSACTION_MAX_EXPIRY_MS)
+    .greater(Joi.ref('auctionLength'))
+    .description(
+      'If set, auction will automatically extend by this length if a bid comes in within 5 minutes before the end of the auction.',
+    )
+    .optional(),
   access: Joi.number()
     .equal(NftAccess.OPEN, NftAccess.MEMBERS)
     .description('Access type of this sale.'),
