e072a71e3ba62b6e39ba5793efee307ecb59bcd2
Boldizsar Mezei <boldizsar@unchained.work>
2023-09-13 08:52:35 +0200

    OTR NFtSetForSale
    Fixes

---
+++ packages/functions/src/services/payment/tangle-service/nft/NftSetForSaleTangleRequestSchema.ts
@@ -0,0 +1,13 @@
+import { NftSetForSaleTangleRequest, TangleRequestType } from '@build-5/interfaces';
+import { baseNftSetForSaleSchema } from '../../../../runtime/firebase/nft/NftSetForSaleRequestSchema';
+import { toJoiObject } from '../../../joi/common';
+import { baseTangleSchema } from '../common';
+
+export const setNftForSaleTangleSchema = toJoiObject<NftSetForSaleTangleRequest>({
+  ...baseTangleSchema(TangleRequestType.NFT_SET_FOR_SALE),
+  ...baseNftSetForSaleSchema,
+})
+  .description('Tangle request object to set an NFT for sale.')
+  .meta({
+    className: 'NftSetForSaleTangleRequest',
+  });
