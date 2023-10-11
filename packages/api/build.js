9d7fcb102b863d8d3d4273f8ac7bd14d0f7a2ce9
Boldizsar Mezei <boldizsar@unchained.work>
2023-10-11 18:59:41 +0200

    Build test

--- packages/api/build.js
+++ packages/api/build.js
@@ -4,9 +4,11 @@ const fs = require('fs');
 delete package.dependencies['@build-5/interfaces'];
 const prodPackageJson = {
   scripts: {
+    preinstall: "./rustinstall.sh",
     start: 'node index',
   },
   dependencies: { ...package.dependencies, '@build-5/database': 'file:./database' },
 };
 
 fs.writeFileSync('./lib/package.json', JSON.stringify(prodPackageJson));
+fs.copyFileSync('../../rustinstall.sh', './lib/rustinstall.sh')
