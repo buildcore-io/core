29827802d935125f34fb6b2ae3fa30e3166363f0
flying rabbit <109557696+flyingrabbit-lab@users.noreply.github.com>
2023-11-20 18:43:58 +0100

    Set ack deadline on subscriptions (#2665)
    * Set ack deadline on subscriptions
    
    Prettier
    
    * Fix mnemonic trigger

--- packages/functions/src/triggers/mnemonic.trigger.ts
+++ packages/functions/src/triggers/mnemonic.trigger.ts
@@ -1,7 +1,11 @@
 import { build5Db } from '@build-5/database';
 import { COL, MAX_WALLET_RETRY, Mnemonic, NetworkAddress, Transaction } from '@build-5/interfaces';
-import { isEmpty } from 'lodash';
+import { chunk, isEmpty } from 'lodash';
 import { FirestoreDocEvent } from './common';
+import {
+  CREDIT_EXECUTABLE_TRANSACTIONS,
+  DEFAULT_EXECUTABLE_TRANSACTIONS,
+} from './transaction-trigger/transaction.trigger';
 
 enum FieldNameType {
   SOURCE_ADDRESS = 'payload.sourceAddress',
@@ -25,16 +29,23 @@ export const onMnemonicUpdated = async (event: FirestoreDocEvent<Mnemonic>) => {
   }
 };
 
+const TYPE_CHUNKS = chunk(DEFAULT_EXECUTABLE_TRANSACTIONS, 10).concat([
+  CREDIT_EXECUTABLE_TRANSACTIONS,
+]);
+
 const getUncofirmedTransactionsId = async (address: NetworkAddress) => {
-  const transactions = await build5Db()
-    .collection(COL.TRANSACTION)
-    .or(Object.values(FieldNameType).map((fieldPath) => ({ fieldPath, value: address })))
-    .where('payload.walletReference.confirmed', '==', false)
-    .where('payload.walletReference.count', '<', MAX_WALLET_RETRY)
-    .limit(1)
-    .get<Transaction>();
-  if (transactions.length) {
-    return transactions[0].uid;
+  for (const types of TYPE_CHUNKS) {
+    const transactions = await build5Db()
+      .collection(COL.TRANSACTION)
+      .or(Object.values(FieldNameType).map((fieldPath) => ({ fieldPath, value: address })))
+      .where('type', 'in', types)
+      .where('payload.walletReference.confirmed', '==', false)
+      .where('payload.walletReference.count', '<', MAX_WALLET_RETRY)
+      .limit(1)
+      .get<Transaction>();
+    if (transactions.length) {
+      return transactions[0].uid;
+    }
   }
   return undefined;
 };
