87e7a1e57ba525f22dbab0c9ae96ce2acd66af1a
Dr-Electron <dr-electr0n@protonmail.com>
2023-12-16 23:25:47 +0100

    Add member examples

---
+++ packages/sdk/examples/member/create.ts
@@ -0,0 +1,18 @@
+import { Build5, https } from '@build-5/sdk';
+import { address } from '../utils/secret';
+
+async function main() {
+  const origin = Build5.TEST;
+  const response = await https(origin)
+    .createMember({
+      address: address.bech32,
+      signature: '',
+      body: {
+        address: address.bech32,
+      },
+    });
+
+  console.log('Member uid: ', response.uid);
+}
+
+main().then(() => process.exit());
