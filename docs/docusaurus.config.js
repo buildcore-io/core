7e03710b7c312296d03821b9184753a530340cba
Dr-Electron <dr-electr0n@protonmail.com>
2024-02-10 21:07:10 +0100

    Update docusaurus to v3.1.1

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -18,6 +18,7 @@ const config = {
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
   onBrokenLinks: 'throw',
+  onBrokenAnchors: 'throw',
   onBrokenMarkdownLinks: 'throw',
   plugins: [
     [
