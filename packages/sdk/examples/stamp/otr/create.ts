2b8fae0efd9c8637cfb3b7573b812cbb21f641f8
Boldizsar Mezei <boldizsar@unchained.work>
2024-02-21 09:36:05 +0100

    Rename Build5OtrAddress to SoonaverseOtrAddress

--- packages/sdk/examples/stamp/otr/create.ts
+++ packages/sdk/examples/stamp/otr/create.ts
@@ -1,14 +1,14 @@
 import { Dataset } from '@build-5/interfaces';
-import { Build5, Build5OtrAddress, otr } from '@build-5/sdk';
+import { Build5, SoonaverseOtrAddress, otr } from '@build-5/sdk';
 
 async function main() {
   const origin = Build5.TEST;
   // @ts-ignore
-  const otrAddress = Build5OtrAddress[origin];
+  const otrAddress = SoonaverseOtrAddress[origin];
 
   console.log('Create stamp under your project...');
   try {
-    const otrRequest = await otr(otrAddress)
+    const otrRequest = otr(otrAddress)
       .dataset(Dataset.STAMP)
       .stamp({ uri: 'https://www.africau.edu/images/default/sample.pdf' });
 
