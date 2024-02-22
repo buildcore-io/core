a5b82a947fcb83ea951e0fc13df179eaedcb1c13
Adam <adam@unchained.work>
2024-02-21 22:46:59 -0800

    one more push

--- packages/functions/scripts/dbUpgrades/2.1/milestone_transactions.ts
+++ packages/functions/scripts/dbUpgrades/2.1/milestone_transactions.ts
@@ -8,6 +8,7 @@ export const milestoneTransaction = async (app: FirebaseApp) => {
   const firestore = instance.firestore();
   let lastDoc: any = undefined;
   let total = 0;
+  // run
   do {
     let query = firestore
       .collectionGroup(SUB_COL.TRANSACTIONS)
