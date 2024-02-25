3dbca2e7665879fcd37378620dcabc7d46b5e7a1
Dr-Electron <dr-electr0n@protonmail.com>
2024-02-25 21:40:12 +0100

    Format

--- packages/sdk/examples/basic_otr.ts
+++ packages/sdk/examples/basic_otr.ts
@@ -2,11 +2,11 @@ import { Dataset } from '@build-5/interfaces';
 import { Build5OtrAddress, otr } from '@build-5/sdk';
 
 async function main() {
-    const otrRequest = otr(Build5OtrAddress.TEST)
-        .dataset(Dataset.NFT)
-        .mintMetadataNft({
-            metadata: { prop1: 'prop1', prop2: 'prop2' },
-        });
-    console.log(otrRequest.getFireflyDeepLink());
+  const otrRequest = otr(Build5OtrAddress.TEST)
+    .dataset(Dataset.NFT)
+    .mintMetadataNft({
+      metadata: { prop1: 'prop1', prop2: 'prop2' },
+    });
+  console.log(otrRequest.getFireflyDeepLink());
 }
 main().then(() => process.exit());
