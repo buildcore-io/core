beef4d0526063d40ea2b19efbe6010456c2374a6
Boldizsar Mezei <boldizsar@unchained.work>
2024-05-02 23:09:02 +0200

    Fixes

--- packages/functions/scripts/dbUpgrades/2.1/migrate_to_pg.ts
+++ packages/functions/scripts/dbUpgrades/2.1/migrate_to_pg.ts
@@ -3,7 +3,6 @@ import { IDocument, Update, database as pgDb, undefinedToNull } from '@buildcore
 import { COL, SUB_COL } from '@buildcore/interfaces';
 import { Firestore } from 'firebase-admin/firestore';
 import { chunk, last } from 'lodash';
-import fs from 'fs'
 
 const collections: { [key: string]: SUB_COL[] } = {
   // [COL.MEMBER]: [],
@@ -19,40 +18,42 @@ const collections: { [key: string]: SUB_COL[] } = {
   // [COL.PROPOSAL]: [SUB_COL.OWNERS, SUB_COL.MEMBERS],
   // [COL.NOTIFICATION]: [],
 
-  // [COL.MILESTONE]: [SUB_COL.TRANSACTIONS],
+  [COL.MILESTONE]: [SUB_COL.TRANSACTIONS],
   // [COL.MILESTONE_RMS]: [SUB_COL.TRANSACTIONS],
-  // [COL.MILESTONE_SMR]: [SUB_COL.TRANSACTIONS],
-  // [COL.TRANSACTION]: [],
+  [COL.MILESTONE_SMR]: [SUB_COL.TRANSACTIONS],
+  [COL.TRANSACTION]: [],
 
-  [COL.TOKEN]: [SUB_COL.STATS, SUB_COL.RANKS, SUB_COL.VOTES, SUB_COL.DISTRIBUTION],
-  [COL.TOKEN_MARKET]: [],
-  [COL.TOKEN_PURCHASE]: [],
-  [COL.TICKER]: [],
-  [COL.STAKE]: [],
-  [COL.STAKE_REWARD]: [],
-  [COL.NFT_STAKE]: [],
-  [COL.AIRDROP]: [],
-  [COL.PROJECT]: [SUB_COL.ADMINS, SUB_COL._API_KEY],
-  [COL.STAMP]: [],
-  [COL.AUCTION]: [],
-  [COL.MNEMONIC]: [],
-  [COL.SYSTEM]: [],
-  [COL.SWAP]: [],
-  [COL.SOON_SNAP]: [],
+  // [COL.TOKEN]: [SUB_COL.STATS, SUB_COL.RANKS, SUB_COL.VOTES, SUB_COL.DISTRIBUTION],
+  // [COL.TOKEN_MARKET]: [],
+  // [COL.TOKEN_PURCHASE]: [],
+  // [COL.TICKER]: [],
+  // [COL.STAKE]: [],
+  // [COL.STAKE_REWARD]: [],
+  // [COL.NFT_STAKE]: [],
+  // [COL.AIRDROP]: [],
+  // [COL.PROJECT]: [SUB_COL.ADMINS, SUB_COL._API_KEY],
+  // [COL.STAMP]: [],
+  // [COL.AUCTION]: [],
+  // [COL.MNEMONIC]: [],
+  // [COL.SYSTEM]: [],
+  // [COL.SWAP]: [],
+  // [COL.SOON_SNAP]: [],
 };
 
 export const migrateToPg = async (app: FirebaseApp) => {
   const firestore: Firestore = app.getInstance().firestore();
 
-  for (const col of Object.keys(collections)) {
+  const promises = Object.keys(collections).map(async (col) => {
     console.log(`Migrating ${col} collection`);
     await migrateColletion(firestore, col as COL);
-  }
+  });
+  await Promise.all(promises);
+
   console.log('Migration done');
   await pgDb().destroy();
 };
 
-const LIMIT = 1000;
+const LIMIT = 10000;
 
 const migrateColletion = async (firestore: Firestore, col: COL) => {
   let lastDoc: any = undefined;
@@ -66,7 +67,7 @@ const migrateColletion = async (firestore: Firestore, col: COL) => {
     const snap = await query.get();
     lastDoc = last(snap.docs);
     count += snap.size;
-    console.log('count', count);
+    console.log('count', col, count);
 
     if (!lastDoc) {
       break;
@@ -74,7 +75,7 @@ const migrateColletion = async (firestore: Firestore, col: COL) => {
 
     const docRef = pgDb().doc(col, 'placeholder') as IDocument<any, any, Update>;
 
-    const promises = chunk(snap.docs, 4).map(async (ch) => {
+    const promises = chunk(snap.docs, 130).map(async (ch) => {
       try {
         const data = ch.map((doc) =>
           undefinedToNull(docRef.converter.toPg({ ...doc.data(), uid: doc.id })),
@@ -112,13 +113,6 @@ const migrateSubDocs = async (firestore: Firestore, col: COL, colId: string, sub
     const data = ch.map((doc) => {
       const docRef = pgDb().doc(col, colId, subCol, doc.id) as IDocument<any, any, Update>;
       const converted = docRef.converter.toPg({ ...doc.data(), uid: doc.id, parentId: colId });
-      if (
-        doc.id === '0x551fd2c7c7bf356bac194587dab2fcd46420054b' &&
-        colId === '0x15e7e6663f3a88c0cce72a4cc3cd5c6786f0b1cf'
-      ) {
-        fs.writeFileSync('original.json', JSON.stringify(doc.data()))
-        fs.writeFileSync('converted.json', JSON.stringify(converted))
-      }
       return converted;
     });
     const docRef = pgDb().doc(col, colId, subCol, 'placeholder') as IDocument<any, any, Update>;
